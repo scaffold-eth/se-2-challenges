f38b52c1b56d4ac20c75c7b33293baf31ffb736e
Shiv Bhonde <shivbhonde04@gmail.com>
2023-06-26 23:33:57 +0530

    update idex with image and 2 paras

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import type { NextPage } from "next";
 import { MetaHeader } from "~~/components/MetaHeader";
 
@@ -6,19 +7,33 @@ const Home: NextPage = () => {
     <>
       <MetaHeader />
       <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center mb-8">
+        <div className="px-5 w-[90%] md:w-[65%]">
+          <h1 className="text-center mb-4">
             <span className="block text-2xl mb-2">SpeedRunEthereum</span>
             <span className="block text-4xl font-bold">Challenge #0: Simple NFT</span>
           </h1>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold">packages/nextjs/pages/index.tsx</code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract <code className="italic bg-base-300 text-base font-bold">YourContract.sol</code> in{" "}
-            <code className="italic bg-base-300 text-base font-bold">packages/hardhat/contracts</code>
-          </p>
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-full h-80 relative mb-4">
+              <Image src="/thumbnail.png" fill alt="challenge banner" />
+            </div>
+            <p className="text-center text-lg">
+              🎫 Create a simple NFT to learn basics of 🏗️ Scaffold-ETH 2. You&apos;ll use 👷‍♀️
+              <a href="https://hardhat.org/getting-started/" target="_blank" rel="noreferrer" className="underline">
+                HardHat
+              </a>{" "}
+              to compile and deploy smart contracts. Then, you&apos;ll use a template React app full of important
+              Ethereum components and hooks. Finally, you&apos;ll deploy an NFT to a public network to share with
+              friends! 🚀
+            </p>
+            <p className="text-center text-lg">
+              🌟 The final deliverable is an app that lets users purchase and transfer NFTs. Deploy your contracts to a
+              testnet then build and upload your app to a public web server. Submit the url on{" "}
+              <a href="https://speedrunethereum.com/" target="_blank" rel="noreferrer" className="underline">
+                SpeedRunEthereum.com
+              </a>{" "}
+              !
+            </p>
+          </div>
         </div>
       </div>
     </>
