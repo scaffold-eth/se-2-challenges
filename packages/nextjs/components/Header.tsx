844ec014f4537c463e5e18728be9514b93c69e8b
Shiv Bhonde <shivbhonde04@gmail.com>
2023-06-26 10:14:15 +0530

    fix weired spaces in Header component

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -87,7 +87,7 @@ export const Header = () => {
   );
 
   return (
-    <div className="sticky   lg:static   top-0 na vbar bg-primary min-h-0 flex-shrink-0 justify-between z-20 s h adow-md shadow-secondary">
+    <div className="sticky lg:static top-0 navbar bg-primary min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary">
       <div className="navbar-start w-auto lg:w-1/2">
         <div className="lg:hidden dropdown" ref={burgerMenuRef}>
           <button
