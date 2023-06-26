33c74be0469d76482a18100584a6e36a15a63120
Carlos Sánchez <oceanrdn@gmail.com>
2023-06-26 11:48:02 +0200

    Logo no-shrink, SE-2 => SRE

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -110,13 +110,13 @@ export const Header = () => {
             </ul>
           )}
         </div>
-        <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6">
+        <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex relative w-10 h-10">
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">SE-2 Challenges</span>
-            <span className="text-xs">#X: Challenge Title</span>
+            <span className="font-bold leading-tight">SRE Challenges</span>
+            <span className="text-xs">#0: Simple NFT</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">{navLinks}</ul>
