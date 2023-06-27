5ee2a2db4070658e4e228b3738f7c7b87e6e81e8
Shiv Bhonde <shivbhonde04@gmail.com>
2023-06-27 22:28:57 +0530

    minor fix to handle high duration

--- packages/nextjs/pages/stakerUI.tsx
+++ packages/nextjs/pages/stakerUI.tsx
@@ -55,17 +55,17 @@ const StakerUI: NextPage = () => {
     <>
       <MetaHeader />
       <div className="flex items-center flex-col flex-grow w-full px-4">
-        <div className="flex flex-col items-center space-y-8 bg-base-100 shadow-lg shadow-secondary border-8 border-secondary rounded-xl p-6 mt-24 w-full max-w-md">
+        <div className="flex flex-col items-center space-y-8 bg-base-100 shadow-lg shadow-secondary border-8 border-secondary rounded-xl p-6 mt-24 w-full max-w-lg">
           <div className="flex flex-col w-full items-center">
             <p className="block text-2xl mt-0 mb-2 font-semibold">Staker Contract</p>
             <Address address={StakerContract?.address} size="xl" />
           </div>
           <div className="flex items-start justify-around w-full">
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center justify-center w-1/2">
               <p className="block text-xl mt-0 mb-1 font-semibold">Time Left</p>
-              <span>{timeLeft ? `${humanizeDuration(timeLeft.toNumber() * 1000)} left` : 0}</span>
+              <p className="m-0 p-0">{timeLeft ? `${humanizeDuration(timeLeft.toNumber() * 1000)} left` : 0}</p>
             </div>
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center w-1/2">
               <p className="block text-xl mt-0 mb-1 font-semibold">You Staked</p>
               <span>
                 {myStake ? utils.formatEther(myStake.toString()) : 0} {configuredNetwork.nativeCurrency.symbol}
@@ -75,7 +75,7 @@ const StakerUI: NextPage = () => {
           <div className="flex flex-col items-center shrink-0 w-full">
             <p className="block text-xl mt-0 mb-1 font-semibold">Total Staked</p>
             <div className="flex space-x-2">
-              {<ETHToPrice value={stakerContractBalance ? stakerContractBalance.toString() : undefined} />}
+              {<ETHToPrice value={stakerContractBalance != null ? stakerContractBalance.toString() : undefined} />}
               <span>/</span>
               {<ETHToPrice value={threshold && utils.formatEther(threshold)} />}
             </div>
