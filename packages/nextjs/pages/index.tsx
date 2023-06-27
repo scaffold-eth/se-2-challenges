cc8d9a85d747ee57664b5406621dd48b58f62506
Carlos Sánchez <oceanrdn@gmail.com>
2023-06-27 10:53:37 +0200

    Home page image size. Max-w for text

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -13,26 +13,32 @@ const Home: NextPage = () => {
             <span className="block text-4xl font-bold">Challenge #0: Simple NFT</span>
           </h1>
           <div className="flex flex-col items-center justify-center">
-            <div className="w-full h-80 relative mb-4 rounded-xl">
-              <Image src="/hero.png" fill alt="challenge banner" className="rounded-xl" />
+            <Image
+              src="/hero.png"
+              width="727"
+              height="231"
+              alt="challenge banner"
+              className="rounded-xl border-4 border-primary"
+            />
+            <div className="max-w-3xl">
+              <p className="text-center text-lg mt-8">
+                🎫 Create a simple NFT to learn basics of 🏗️ Scaffold-ETH 2. You&apos;ll use 👷‍♀️
+                <a href="https://hardhat.org/getting-started/" target="_blank" rel="noreferrer" className="underline">
+                  HardHat
+                </a>{" "}
+                to compile and deploy smart contracts. Then, you&apos;ll use a template React app full of important
+                Ethereum components and hooks. Finally, you&apos;ll deploy an NFT to a public network to share with
+                friends! 🚀
+              </p>
+              <p className="text-center text-lg">
+                🌟 The final deliverable is an app that lets users purchase and transfer NFTs. Deploy your contracts to
+                a testnet then build and upload your app to a public web server. Submit the url on{" "}
+                <a href="https://speedrunethereum.com/" target="_blank" rel="noreferrer" className="underline">
+                  SpeedRunEthereum.com
+                </a>{" "}
+                !
+              </p>
             </div>
-            <p className="text-center text-lg">
-              🎫 Create a simple NFT to learn basics of 🏗️ Scaffold-ETH 2. You&apos;ll use 👷‍♀️
-              <a href="https://hardhat.org/getting-started/" target="_blank" rel="noreferrer" className="underline">
-                HardHat
-              </a>{" "}
-              to compile and deploy smart contracts. Then, you&apos;ll use a template React app full of important
-              Ethereum components and hooks. Finally, you&apos;ll deploy an NFT to a public network to share with
-              friends! 🚀
-            </p>
-            <p className="text-center text-lg">
-              🌟 The final deliverable is an app that lets users purchase and transfer NFTs. Deploy your contracts to a
-              testnet then build and upload your app to a public web server. Submit the url on{" "}
-              <a href="https://speedrunethereum.com/" target="_blank" rel="noreferrer" className="underline">
-                SpeedRunEthereum.com
-              </a>{" "}
-              !
-            </p>
           </div>
         </div>
       </div>
