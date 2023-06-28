5a631900ef1409224fe3d3b4c5dd978a0e9331f7
Shiv Bhonde <shivbhonde04@gmail.com>
2023-06-28 20:44:06 +0530

    minor tweak to Staker.sol comment

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -13,7 +13,7 @@ contract Staker {
   }
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  // ( Make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+  // (Make sure to add a `Stake(address,uint256)` event and emit it for the frontend `All Stakings` tab to display)
 
 
   // After some `deadline` allow anyone to call an `execute()` function
