838b209a45f76ae87a77dbdb8b3824bfad30a31f
mertcanciy <59885513+mertcanciy@users.noreply.github.com>
2023-07-08 20:47:48 +0300

    Left Part of index.tsx

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ This challenge will help you build/understand a simple decentralized exchange, w
 🌟 The final deliverable is an app that {challengeDeliverable}.
 Deploy your contracts to a testnet then build and upload your app to a public web server. Submit the url on [SpeedRunEthereum.com](https://speedrunethereum.com)!
 
-💬 Meet other builders working on this challenge and get help in the {challengeTelegramLink}
+There is also a 🎥 [Youtube video](https://www.youtube.com/watch?v=eP5w6Ger1EQ&t=364s&ab_channel=SimplyExplained) that may help you understand the concepts covered within this challenge too:
+
+💬 Meet other builders working on this challenge and get help in the [Challenge 4 Telegram](https://t.me/+_NeUIJ664Tc1MzIx)
 
 ## Checkpoint 0: 📦 Install 📚
 
@@ -46,6 +48,15 @@ yarn start
 
 📱 Open http://localhost:3000 to see the app.
 
-## Checkpoint {num}: Create all the required Checkpoints for the challenge
+## ⛳️ Checkpoint 1: 🔭 The Structure 📺
+Navigate to the Debug Contracts tab and you should see two smart contracts displayed called DEX and Balloons.
+
+👩‍💻 Rerun yarn deploy whenever you want to deploy new contracts to the frontend (run yarn deploy --reset for a completely fresh deploy if you have made no contract changes).
+
+Balloons.sol is just an example ERC20 contract that mints 1000 $BAL to whatever address deploys it. DEX.sol is what we will build in this challenge and you can see it starts with a SafeMath library to help us prevent overflows and underflows and also tracks a token (ERC20 interface) that we set in the constructor (on deploy).
+
+Below is what your front-end will look like with no implementation code within your smart contracts yet. The buttons will likely break because there are no functions tied to them yet!
+
+⭐️ Also note that there is no curve until you uncomment the specific lines of code at the end of hardhat/deploy/00_deploy_your_contract.ts.
 
 ## ⚔️ Side Quests
--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,15 +1,167 @@
+import { BigNumber, ethers } from "ethers";
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 import { MetaHeader } from "~~/components/MetaHeader";
+import { Address, Balance, EtherInput } from "~~/components/scaffold-eth";
+import { useDeployedContractInfo, useScaffoldContract, useScaffoldContractRead, useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 
 const Home: NextPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [ethToTokenAmount, setEthToTokenAmount] = useState("");
+  const [tokenToETHAmount, setTokenToETHAmount] = useState("");
+  const [depositAmount, setDepositAmount] = useState("");
+  const [withdrawAmount, setWithdrawAmount] = useState("");
+  const [approveSpender, setApproveSpender] = useState("");
+  const [approveAmount, setApproveAmount] = useState("");
+  const [accountBalanceOf, setAccountBalanceOf] = useState("");
+
+
+
+  const {data: DEXInfo} = useDeployedContractInfo("DEX");
+  const {data: BalloonsInfo} = useDeployedContractInfo("Balloons");
+
+  
+  const {data: DEXBalloonBalance} = useScaffoldContractRead({
+    contractName:"Balloons",
+    functionName:"balanceOf",
+    args:[DEXInfo?.address?.toString()]
+  });
+
+  useEffect(() => {
+    if (DEXBalloonBalance !== undefined) {
+      setIsLoading(false);
+    }
+  }, [DEXBalloonBalance]);
+
+  const { writeAsync: ethToTokenWrite } = useScaffoldContractWrite({
+    contractName: "DEX",
+    functionName: "ethToToken",
+    value: ethToTokenAmount
+  });
+
+  const { writeAsync: tokenToEthWrite } = useScaffoldContractWrite({
+    contractName: "DEX",
+    functionName: "tokenToEth",
+    args: [ethers.utils.parseEther(tokenToETHAmount || "0")]
+  });
+
+  const {writeAsync: depositWrite} = useScaffoldContractWrite({
+    contractName:"DEX",
+    functionName:"deposit",
+    value: depositAmount
+  });
+
+  const {writeAsync: withdrawWrite} = useScaffoldContractWrite({
+    contractName:"DEX",
+    functionName:"withdraw",
+    args: [ethers.utils.parseEther(withdrawAmount || "0")]
+  });
+
+  const {writeAsync: approveWrite} = useScaffoldContractWrite({
+    contractName:"Balloons",
+    functionName:"approve",
+    args: [approveSpender, ethers.utils.parseEther(approveAmount || "0")]
+  });
+
+  const {data: balanceOfWrite} = useScaffoldContractRead({
+    contractName:"Balloons",
+    functionName:"balanceOf",
+    args: [accountBalanceOf]
+  });
+  console.log("hey",balanceOfWrite);
+
   return (
     <>
       <MetaHeader />
-      <div className="flex items-center flex-col flex-grow pt-10">
+      <h1 className="text-center mb-4 mt-9">
+            <span className="block text-2xl mb-2">SpeedRunEthereum</span>
+            <span className="block text-4xl font-bold">Challenge 4: Minimum Viable Exchange </span>
+          </h1>
+      <div className="flex items-center flex-col flex-grow pt-10 grid grid-cols-2">
+        <div className="px-5 border rounded-3xl py-5 ml-3">
+              <div className="flex text-center mb-8 px-4 gap-4">
+                <span className="block text-2xl mb-2"><Address address={DEXInfo?.address}/></span>
+                <span><Balance address={DEXInfo?.address}/></span>
+                {isLoading ? (
+                  <span>Loading...</span>
+                ) : (
+                  <span>Balloons: {ethers.utils.formatEther(DEXBalloonBalance?.toString() || 0)}</span>
+                )}
+              </div>
+              <div className="border rounded-3xl py-3 px-4">
+              <div className="flex mb-4">
+                <span>ethToToken  <EtherInput value={ethToTokenAmount} onChange={(value) => setEthToTokenAmount(value)}/></span>
+                <button className="btn btn-primary h-[2.2rem] min-h-[2.2rem] mt-6 mx-5" onClick={ethToTokenWrite}>Send</button>
+              </div>
+              <div className="flex">
+                <span>tokenToETH  <EtherInput value={tokenToETHAmount} onChange={(value) => setTokenToETHAmount(value)}/></span>
+                <button className="btn btn-primary h-[2.2rem] min-h-[2.2rem] mt-6 mx-5" onClick={tokenToEthWrite}>Send</button>
+              </div>
+            </div>
+            <div className="relative flex py-5 items-center">
+                <div className="flex-grow border-t border-gray-400"></div>
+                <span className="flex-shrink mx-4 text-gray-400">Liquidity(None)</span>
+                <div className="flex-grow border-t border-gray-400"></div>
+            </div>
+
+            <div className="border rounded-3xl px-4 py-3">
+              <div className="flex mb-4">
+                <span>Deposit  <EtherInput value={depositAmount} onChange={(value) => setDepositAmount(value)}/></span>
+                <button className="btn btn-primary h-[2.2rem] min-h-[2.2rem] mt-6 mx-5" onClick={depositWrite}>Send</button>
+              </div>
+              <div className="flex">
+                <span>Withdraw  <EtherInput value={withdrawAmount} onChange={(value) => setWithdrawAmount(value)}/></span>
+                <button className="btn btn-primary h-[2.2rem] min-h-[2.2rem] mt-6 mx-5" onClick={withdrawWrite}>Send</button>
+
+              </div>
+            </div>
+
+            <div className="relative flex pt-8 items-center">
+                <div className="flex-grow border-t border-gray-400"></div>
+                <div className="flex-grow border-t border-gray-400"></div>
+            </div>
+
+            <div className="flex text-center mt-8 gap-5 mb-5">
+                <span className="block text-2xl mb-2">Balloons</span>
+                <span className="ml-8"><Address address={BalloonsInfo?.address}/></span>
+            </div>
+
+            <div className="border rounded-3xl px-4 py-3 grid grid-cols-2">
+
+              <div className="flex flex-col gap-4 mb-4">
+                <span>Approve  <EtherInput value={approveSpender} onChange={(value) => setApproveSpender(value)} placeholder="Address Spender"/></span>
+                <EtherInput value={approveAmount} onChange={(value) => setApproveAmount(value)} placeholder="uint256 Amount"/>
+              </div>
+
+              <div className=" my-auto">
+              <button className="btn btn-primary h-[2.2rem] min-h-[2.2rem] mt-auto ml-5" onClick={approveWrite}>Send</button>
+              </div>
+  
+            </div>
+
+            <div className="border rounded-3xl px-4 py-3 mt-6">
+              <div className="flex mb-4">
+                <span className="flex flex-col mt-2">balanceOf <input className="pl-4 py-4 px-8 text-accent self-center bg-transparent border border-blue-100 rounded-2xl h-[2.2rem] min-h-[2.2rem]" value={accountBalanceOf} onChange={(event) => setAccountBalanceOf(event.target.value)} placeholder="address Account"/></span>                
+              </div>
+              {balanceOfWrite === undefined
+                ? (
+                  <h1></h1>
+                ):
+                (
+                  <span>{ethers.utils.formatEther(balanceOfWrite || BigNumber.from("0"))}</span>
+                )
+                }
+            </div>
+        </div>
+
+        
+
+
+
         <div className="px-5">
           <h1 className="text-center mb-8">
             <span className="block text-2xl mb-2">SpeedRunEthereum</span>
-            <span className="block text-4xl font-bold">Challenge #X: Challenge Title </span>
+            <span className="block text-4xl font-bold">Challenge 4: Minimum Viable Exchange </span>
           </h1>
           <p className="text-center text-lg">
             Get started by editing{" "}
