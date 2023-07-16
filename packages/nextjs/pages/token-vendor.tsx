c44f37ffbcb49fcc96285f5e02ad1151a9c355b0
Rinat <rinat@hey.com>
2023-07-16 17:38:15 +0700

    feat: buy tokens

--- packages/nextjs/pages/token-vendor.tsx
+++ packages/nextjs/pages/token-vendor.tsx
@@ -4,12 +4,14 @@ import { formatEther } from "ethers/lib/utils.js";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
 import { MetaHeader } from "~~/components/MetaHeader";
-import { AddressInput, IntegerInput } from "~~/components/scaffold-eth";
+import { AddressInput, EtherInput, IntegerInput, IntegerVariant, isValidInteger } from "~~/components/scaffold-eth";
 import { useScaffoldContractRead, useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 
 const TokenVendor: NextPage = () => {
   const [toAddress, setToAddress] = useState("");
-  const [tokensToSend, setTokensToSend] = useState<string | BigNumber>("");
+  const [tokensToSend, setTokensToSend] = useState("");
+  const [tokensToBuy, setTokensToBuy] = useState<string | BigNumber>("");
+
   const { address } = useAccount();
   const { data: yourTokenSymbol } = useScaffoldContractRead({
     contractName: "YourToken",
@@ -22,10 +24,19 @@ const TokenVendor: NextPage = () => {
     args: [address],
   });
 
-  const { writeAsync } = useScaffoldContractWrite({
+  const { writeAsync: transferTokens } = useScaffoldContractWrite({
     contractName: "YourToken",
     functionName: "transfer",
-    args: [toAddress, BigNumber.from(tokensToSend || 0)],
+    args: [
+      toAddress,
+      BigNumber.from(tokensToSend && isValidInteger(IntegerVariant.UINT256, tokensToSend, false) ? tokensToSend : 0),
+    ],
+  });
+
+  const { writeAsync: buyTokens } = useScaffoldContractWrite({
+    contractName: "Vendor",
+    functionName: "buyTokens",
+    value: tokensToBuy.toString(),
   });
 
   return (
@@ -40,21 +51,39 @@ const TokenVendor: NextPage = () => {
           </div>
         </div>
 
-        <div className="flex flex-col items-center space-y-2 bg-base-100 shadow-lg shadow-secondary border-8 border-secondary rounded-xl p-6 mt-8 w-full max-w-lg">
-          <div className="text-xl">Transfer tokens</div>
+        <div className="flex flex-col items-center space-y-4 bg-base-100 shadow-lg shadow-secondary border-8 border-secondary rounded-xl p-6 mt-8 w-full max-w-lg">
+          <div className="text-xl">Buy tokens</div>
           <div className="w-full flex flex-col space-y-2">
-            <AddressInput placeholder="to address" value={toAddress} onChange={value => setToAddress(value)} />
-            <IntegerInput
-              placeholder="amount of tokens to send"
-              value={tokensToSend}
-              onChange={value => setTokensToSend(value)}
+            <EtherInput
+              // placeholder="amount of tokens to buy"
+              placeholder="amount of ETH or USD payed"
+              value={tokensToBuy.toString()}
+              onChange={value => setTokensToBuy(value)}
             />
           </div>
 
-          <button className="btn btn-secondary" onClick={() => writeAsync()}>
-            Send Tokens
+          <button className="btn btn-secondary mt-2" onClick={() => buyTokens()}>
+            Buy Tokens
           </button>
         </div>
+
+        {yourTokenBalance && !yourTokenBalance?.isZero() && (
+          <div className="flex flex-col items-center space-y-4 bg-base-100 shadow-lg shadow-secondary border-8 border-secondary rounded-xl p-6 mt-8 w-full max-w-lg">
+            <div className="text-xl">Transfer tokens</div>
+            <div className="w-full flex flex-col space-y-2">
+              <AddressInput placeholder="to address" value={toAddress} onChange={value => setToAddress(value)} />
+              <IntegerInput
+                placeholder="amount of tokens to send"
+                value={tokensToSend}
+                onChange={value => setTokensToSend(value as string)}
+              />
+            </div>
+
+            <button className="btn btn-secondary" onClick={() => transferTokens()}>
+              Send Tokens
+            </button>
+          </div>
+        )}
       </div>
     </>
   );
