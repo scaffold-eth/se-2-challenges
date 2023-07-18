0b80c277f934fdc882bafba75b544cb68df07b6c
Rinat <rinat@hey.com>
2023-07-18 14:36:50 +0700

    fix: comment solidity code

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -5,15 +5,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
 contract Vendor is Ownable {
-  // TODO: comment
-  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  // event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
   // TODO: remove
-  event SellTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  // event SellTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
   YourToken public yourToken;
 
   // TODO remove
-  uint256 public constant tokensPerEth = 100;
+  // uint256 public constant tokensPerEth = 100;
 
   constructor(address tokenAddress) {
     yourToken = YourToken(tokenAddress);
@@ -21,30 +20,31 @@ contract Vendor is Ownable {
 
   // ToDo: create a payable buyTokens() function:
   // TODO remove
-  function buyTokens() external payable {
-    uint256 tokens = (msg.value * tokensPerEth);
-    yourToken.transfer(msg.sender, tokens);
+  // function buyTokens() external payable {
+  //   uint256 tokens = (msg.value * tokensPerEth);
+  //   yourToken.transfer(msg.sender, tokens);
 
-    emit BuyTokens(msg.sender, msg.value, tokens);
-  }
+  //   emit BuyTokens(msg.sender, msg.value, tokens);
+  // }
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
   // TODO remove
-  function withdraw() external onlyOwner {
-    (bool success, ) = owner().call{value: address(this).balance}("");
-    require(success, "Something went wrong Withdraw");
-  }
+  // function withdraw() external onlyOwner {
+  //   (bool success, ) = owner().call{value: address(this).balance}("");
+  //   require(success, "Something went wrong Withdraw");
+  // }
 
   // ToDo: create a sellTokens(uint256 _amount) function:
-  function sellTokens(uint256 _amount) external {
-    uint256 ethValue = (_amount / tokensPerEth);
-    require(ethValue <= address(this).balance, "Inusfficient funds");
+  // TODO remove
+  // function sellTokens(uint256 _amount) external {
+  //   uint256 ethValue = (_amount / tokensPerEth);
+  //   require(ethValue <= address(this).balance, "Inusfficient funds");
 
-    yourToken.transferFrom(msg.sender, address(this), _amount);
+  //   yourToken.transferFrom(msg.sender, address(this), _amount);
 
-    (bool success, ) = msg.sender.call{value: ethValue}("");
-    require(success, "Something went wrong Sell");
+  //   (bool success, ) = msg.sender.call{value: ethValue}("");
+  //   require(success, "Something went wrong Sell");
 
-    emit SellTokens(msg.sender, ethValue, _amount);
-  }
+  //   emit SellTokens(msg.sender, ethValue, _amount);
+  // }
 }
--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -8,7 +8,5 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract YourToken is ERC20 {
   constructor() ERC20("Gold", "GLD") {
     //_mint( ~~~YOUR FRONTEND ADDRESS HERE~~~~ , 1000 * 10 ** 18);
-    // TODO remove
-    _mint(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266, 1000 * 10 ** 18);
   }
 }
