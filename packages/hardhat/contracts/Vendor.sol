a415939e4f4d8b415de6572bc4e12ef75d142e2d
Rinat <rinat@hey.com>
2023-07-18 23:39:39 +0700

    fix(solidity): remove vendor methods

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -6,45 +6,16 @@ import "./YourToken.sol";
 
 contract Vendor is Ownable {
   // event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
-  // TODO: remove
-  // event SellTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
   YourToken public yourToken;
-
-  // TODO remove
-  // uint256 public constant tokensPerEth = 100;
 
   constructor(address tokenAddress) {
     yourToken = YourToken(tokenAddress);
   }
 
   // ToDo: create a payable buyTokens() function:
-  // TODO remove
-  // function buyTokens() external payable {
-  //   uint256 tokens = (msg.value * tokensPerEth);
-  //   yourToken.transfer(msg.sender, tokens);
-
-  //   emit BuyTokens(msg.sender, msg.value, tokens);
-  // }
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
-  // TODO remove
-  // function withdraw() external onlyOwner {
-  //   (bool success, ) = owner().call{value: address(this).balance}("");
-  //   require(success, "Something went wrong Withdraw");
-  // }
 
   // ToDo: create a sellTokens(uint256 _amount) function:
-  // TODO remove
-  // function sellTokens(uint256 _amount) external {
-  //   uint256 ethValue = (_amount / tokensPerEth);
-  //   require(ethValue <= address(this).balance, "Inusfficient funds");
-
-  //   yourToken.transferFrom(msg.sender, address(this), _amount);
-
-  //   (bool success, ) = msg.sender.call{value: ethValue}("");
-  //   require(success, "Something went wrong Sell");
-
-  //   emit SellTokens(msg.sender, ethValue, _amount);
-  // }
 }
