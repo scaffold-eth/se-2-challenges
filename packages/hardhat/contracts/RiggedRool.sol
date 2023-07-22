f0d301b14f1dd6e92da2bfa2cd681a80bf638f72
Angry Coding <mailidanielifechukwu@gmail.com>
2023-07-22 10:59:16 +0100

    Contracts
    This are the contracts

---
+++ packages/hardhat/contracts/RiggedRool.sol
@@ -0,0 +1,24 @@
+pragma solidity >=0.8.0 <0.9.0;  //Do not change the solidity version as it negativly impacts submission grading
+//SPDX-License-Identifier: MIT
+
+import "hardhat/console.sol";
+import "./DiceGame.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract RiggedRoll is Ownable {
+
+    DiceGame public diceGame;
+
+    constructor(address payable diceGameAddress) {
+        diceGame = DiceGame(diceGameAddress);
+    }
+
+    //Add withdraw function to transfer ether from the rigged contract to an address
+
+
+    //Add riggedRoll() function to predict the randomness in the DiceGame contract and only roll when it's going to be a winner
+
+
+    //Add receive() function so contract can receive Eth
+    
+}
