6e8a97fd422811f803c5d21595637cc0735521d3
Shiv Bhonde <shivbhonde04@gmail.com>
2023-08-03 00:14:00 +0530

    add sellEvents UI

--- packages/nextjs/pages/events.tsx
+++ packages/nextjs/pages/events.tsx
@@ -6,25 +6,34 @@ import { Address } from "~~/components/scaffold-eth";
 import { useScaffoldEventHistory } from "~~/hooks/scaffold-eth";
 
 const Events: NextPage = () => {
-  const { data: buyTokenEvents, isLoading: isBuyEventsLoading } = useScaffoldEventHistory({
-    contractName: "Vendor",
-    eventName: "BuyTokens",
-    fromBlock: 0,
-  });
+  // // BuyTokens Events
+  // const { data: buyTokenEvents, isLoading: isBuyEventsLoading } = useScaffoldEventHistory({
+  //   contractName: "Vendor",
+  //   eventName: "BuyTokens",
+  //   fromBlock: 0,
+  // });
+
+  // // SellTokens Events
+  // const { data: sellTokenEvents, isLoading: isSellEventsLoading } = useScaffoldEventHistory({
+  //   contractName: "Vendor",
+  //   eventName: "SellTokens",
+  //   fromBlock: 0,
+  // });
 
   return (
     <>
       <MetaHeader />
       <div className="flex items-center flex-col flex-grow pt-10">
-        {isBuyEventsLoading ? (
-          <div className="flex justify-center items-center mt-10">
-            <Spinner width="75" height="75" />
+        {/* BuyTokens Events */}
+        {/* <div>
+          <div className="text-center mb-4">
+            <span className="block text-2xl font-bold">Buy Token Events</span>
           </div>
-        ) : (
-          <div>
-            <div className="text-center mb-4">
-              <span className="block text-2xl font-bold">Buy Token Events</span>
+          {isBuyEventsLoading ? (
+            <div className="flex justify-center items-center mt-8">
+              <Spinner width="65" height="65" />
             </div>
+          ) : (
             <div className="overflow-x-auto shadow-lg">
               <table className="table table-zebra w-full">
                 <thead>
@@ -57,8 +66,53 @@ const Events: NextPage = () => {
                 </tbody>
               </table>
             </div>
+          )}
+        </div> */}
+
+        {/* SellTokens Events */}
+        {/* <div className="mt-14">
+          <div className="text-center mb-4">
+            <span className="block text-2xl font-bold">Sell Token Events</span>
           </div>
-        )}
+          {isSellEventsLoading ? (
+            <div className="flex justify-center items-center mt-8">
+              <Spinner width="65" height="65" />
+            </div>
+          ) : (
+            <div className="overflow-x-auto shadow-lg">
+              <table className="table table-zebra w-full">
+                <thead>
+                  <tr>
+                    <th className="bg-primary">Seller</th>
+                    <th className="bg-primary">Amount of Tokens</th>
+                    <th className="bg-primary">Amount of ETH</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {!sellTokenEvents || sellTokenEvents.length === 0 ? (
+                    <tr>
+                      <td colSpan={3} className="text-center">
+                        No events found
+                      </td>
+                    </tr>
+                  ) : (
+                    sellTokenEvents?.map((event, index) => {
+                      return (
+                        <tr key={index}>
+                          <td className="text-center">
+                            <Address address={event.args.seller} />
+                          </td>
+                          <td>{utils.formatEther(event.args.amountOfTokens).toString()}</td>
+                          <td>{utils.formatEther(event.args.amountOfETH).toString()}</td>
+                        </tr>
+                      );
+                    })
+                  )}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div> */}
       </div>
     </>
   );
