3631848aa8ff7073732be02b93b048e8124babf7
Angry Coding <mailidanielifechukwu@gmail.com>
2023-08-08 22:13:36 +0100

    chore: Added header icon

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Bars3Icon, BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, BugAntIcon, CubeIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
@@ -40,9 +40,12 @@ export const Header = () => {
         <NavLink href="/">Home</NavLink>
       </li>
       <li>
-        <NavLink href="/dice">Dice Game</NavLink>
+        <NavLink href="/dice">
+          <CubeIcon className="h-4 w-4" />
+          Dice Game
+        </NavLink>
       </li>
-      <li>
+      <li> 
         <NavLink href="/debug">
           <BugAntIcon className="h-4 w-4" />
           Debug Contracts
