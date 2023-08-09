a5f07dbabdec175ce66ba859304b7756de2d3ec5
Zak G <zakgriffith@gmail.com>
2023-08-09 11:12:29 -0600

    Clean comments in Streamer.sol

--- packages/hardhat/contracts/Streamer.sol
+++ packages/hardhat/contracts/Streamer.sol
@@ -15,13 +15,13 @@ contract Streamer is Ownable {
 
   function fundChannel() public payable {
     /*
-        Checkpoint 2: fund a channel
+      Checkpoint 2: fund a channel
 
-        complete this function so that it:
-        - reverts if msg.sender already has a running channel (ie, if balances[msg.sender] != 0)
-        - updates the balances mapping with the eth received in the function call
-        - emits an Opened event
-        */
+      Complete this function so that it:
+      - reverts if msg.sender already has a running channel (ie, if balances[msg.sender] != 0)
+      - updates the balances mapping with the eth received in the function call
+      - emits an Opened event
+    */
   }
 
   function timeLeft(address channel) public view returns (uint256) {
@@ -45,40 +45,40 @@ contract Streamer is Ownable {
     // `reimburseService` and `processVoucher`. Note that those ethers signing and verification
     // functions do the same under the hood.
     //
-    // again, see https://blog.ricmoo.com/verifying-messages-in-solidity-50a94f82b2ca
+    // see https://blog.ricmoo.com/verifying-messages-in-solidity-50a94f82b2ca
     bytes memory prefixed = abi.encodePacked("\x19Ethereum Signed Message:\n32", hashed);
     bytes32 prefixedHashed = keccak256(prefixed);
 
     /*
-        Checkpoint 4: Recover earnings
+      Checkpoint 4: Recover earnings
 
-        The service provider would like to cash out their hard earned ether.
-            - use ecrecover on prefixedHashed and the supplied signature
-            - require that the recovered signer has a running channel with balances[signer] > v.updatedBalance
-            - calculate the payment when reducing balances[signer] to v.updatedBalance
-            - adjust the channel balance, and pay the contract owner. (Get the owner address withthe `owner()` function)
-            - emit the Withdrawn event
-        */
+      The service provider would like to cash out their hard earned ether.
+          - use ecrecover on prefixedHashed and the supplied signature
+          - require that the recovered signer has a running channel with balances[signer] > v.updatedBalance
+          - calculate the payment when reducing balances[signer] to v.updatedBalance
+          - adjust the channel balance, and pay the Guru(Contract owner). Get the owner address with the `owner()` function.
+          - emit the Withdrawn event
+    */
   }
 
   /*
     Checkpoint 5a: Challenge the channel
 
-    create a public challengeChannel() function that:
+    Create a public challengeChannel() function that:
     - checks that msg.sender has an open channel
     - updates canCloseAt[msg.sender] to some future time
     - emits a Challenged event
-    */
+  */
 
   /*
     Checkpoint 5b: Close the channel
 
-    create a public defundChannel() function that:
+    Create a public defundChannel() function that:
     - checks that msg.sender has a closing channel
     - checks that the current time is later than the closing time
     - sends the channel's remaining funds to msg.sender, and sets the balance to 0
     - emits the Closed event
-    */
+  */
 
   struct Voucher {
     uint256 updatedBalance;
