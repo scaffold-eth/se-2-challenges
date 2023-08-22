f1f49990c5717d8e70cd1536130f632ae8e6fcf4
Shiv Bhonde <shivbhonde04@gmail.com>
2023-08-22 17:26:53 +0530

    fix displayVariables color

--- packages/nextjs/components/scaffold-eth/Contract/DisplayVariable.tsx
+++ packages/nextjs/components/scaffold-eth/Contract/DisplayVariable.tsx
@@ -45,7 +45,7 @@ export const DisplayVariable = ({ contractAddress, abiFunction, refreshDisplayVa
           )}
         </button>
       </div>
-      <div className="text-gray-500 font-medium flex flex-col items-start">
+      <div className="text-secondary-content font-medium flex flex-col items-start">
         <div>
           <div
             className={`break-all block transition bg-transparent ${
