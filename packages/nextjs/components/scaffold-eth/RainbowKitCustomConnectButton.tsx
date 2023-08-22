bf54d52878f47b8ec207b496e61990c2c6df466f
Shiv Bhonde <shivbhonde04@gmail.com>
2023-08-22 17:42:42 +0530

    fix some colors for notification and dropdowns

--- packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton.tsx
+++ packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton.tsx
@@ -99,7 +99,7 @@ export const RainbowKitCustomConnectButton = () => {
                     </label>
                     <ul
                       tabIndex={0}
-                      className="dropdown-content menu p-2 mt-1 shadow-center shadow-accent bg-base-200 rounded-box gap-1"
+                      className="dropdown-content menu p-2 mt-1 shadow-center shadow-accent bg-secondary rounded-box gap-1"
                     >
                       <li>
                         {addressCopied ? (
