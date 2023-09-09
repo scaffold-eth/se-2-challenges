b025617e22ac350bd421f0243278eda74b7542ed
Rinat <rinat@hey.com>
2023-09-09 19:01:44 +0400

    feat: riggedRoll

--- packages/nextjs/pages/dice.tsx
+++ packages/nextjs/pages/dice.tsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
 import type { NextPage } from "next";
-import { Address } from "viem";
+import { createTestClient, http } from "viem";
+import { hardhat } from "viem/chains";
+import { useBalance, useChainId } from "wagmi";
 import { Activities } from "~~/components/Activities";
 import { TActivityItemProps } from "~~/components/ActivitiesItem";
 import { Dice } from "~~/components/Dice";
 import { MetaHeader } from "~~/components/MetaHeader";
 import { Tab } from "~~/components/Tab";
 import { Winners } from "~~/components/Winner";
+import { Address } from "~~/components/scaffold-eth";
 import {
   useAccountBalance,
+  useScaffoldContract,
   useScaffoldContractRead,
   useScaffoldContractWrite,
   useScaffoldEventHistory,
@@ -16,9 +20,12 @@ import {
 } from "~~/hooks/scaffold-eth";
 
 const ROLL_ETH_VALUE = "0.002";
+const CHANGE_BLOCKS_INTERVAL_MS = 1000;
 
 const DiceGame: NextPage = () => {
   const [rolls, setRolls] = useState<TActivityItemProps[]>([]);
+  const { data: riggedRollContract } = useScaffoldContract({ contractName: "RiggedRoll" });
+  const { data: riggedRollBalance } = useBalance({ address: riggedRollContract?.address, watch: true });
 
   const { data: rollsHistoryData, isLoading: rollsHistoryLoading } = useScaffoldEventHistory({
     contractName: "DiceGame",
@@ -27,7 +34,7 @@ const DiceGame: NextPage = () => {
   });
 
   useEffect(() => {
-    if (!rolls.length && !rollsHistoryLoading) {
+    if (!rolls.length && !!rollsHistoryData?.length && !rollsHistoryLoading) {
       setRolls(
         rollsHistoryData?.map(({ args }) => ({
           address: args.player,
@@ -38,6 +45,18 @@ const DiceGame: NextPage = () => {
     }
   }, [rolls, rollsHistoryData, rollsHistoryLoading]);
 
+  useEffect(() => {
+    try {
+      createTestClient({
+        chain: hardhat,
+        mode: "hardhat",
+        transport: http(),
+      })?.setIntervalMining({
+        interval: CHANGE_BLOCKS_INTERVAL_MS,
+      });
+    } catch (e) {}
+  }, []);
+
   useScaffoldEventSubscriber({
     contractName: "DiceGame",
     eventName: "Roll",
@@ -47,8 +66,8 @@ const DiceGame: NextPage = () => {
 
         if (player && amount && roll) {
           setRolls(rolls => [
-            ...rolls,
             { address: player, amount: Number(amount), landedOn: roll.toString(16).toUpperCase() },
+            ...rolls,
           ]);
         }
       });
@@ -61,6 +80,11 @@ const DiceGame: NextPage = () => {
     value: ROLL_ETH_VALUE,
   });
 
+  const { writeAsync: riggedRoll } = useScaffoldContractWrite({
+    contractName: "RiggedRoll",
+    functionName: "riggedRoll",
+  });
+
   return (
     <>
       <MetaHeader />
@@ -69,17 +93,21 @@ const DiceGame: NextPage = () => {
           <div className="w-1/3">
             <Activities rolls={rolls} />
           </div>
-          <div className="w-1/3">
+          <div className="w-1/3 flex flex-col items-center">
             <Dice />
             <button onClick={() => randomDiceRoll()} className="btn btn-secondary btn-xl normal-case font-xl text-lg">
               Roll the dice
             </button>
-            <div className="mt-6 flex w-full justify-center ">
-              <span className="text-xl"> This button allow a rigged roll </span>
+            <div className="mt-4 pt-4 flex flex-col items-center w-full justify-center border-t-4 border-primary">
+              <span className="text-2xl">Rigged Roll</span>
+              <div className="flex my-2">
+                <Address address={riggedRollContract?.address} />{" "}
+                <span className="ml-4">{riggedRollBalance?.formatted}</span>
+              </div>
             </div>
-            {/* <button onClick={onRoll} className="btn btn-secondary btn-xl normal-case font-xl text-lg">
-          Rigged Roll
-        </button> */}
+            <button onClick={() => riggedRoll()} className="mt-4 btn btn-secondary btn-xl normal-case font-xl text-lg">
+              Rigged Roll
+            </button>
           </div>
           {/* <div className="w-1/3">
             <Winners winners={won} />
