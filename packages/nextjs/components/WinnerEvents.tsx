aa04a255ef6d9fd02902085d36a3e8b4c2473fcd
Rinat <rinat@hey.com>
2023-09-11 21:51:33 +0400

    fix: winner events table

--- packages/nextjs/components/WinnerEvents.tsx
+++ packages/nextjs/components/WinnerEvents.tsx
@@ -19,21 +19,21 @@ export const WinnerEvents = ({ winners }: WinnerEventsProps) => {
         <p className="flex justify-center text-lg font-bold">Winner Events</p>
       </div>
       <div className="p-2 flex flex-col mt-0">
-        <div className="grid grid-cols-4 text-lg">
+        <div className="grid grid-cols-5 text-lg">
           <div className="col-span-3">
             <span>Address</span>
           </div>
-          <div className="col-span-1">
+          <div className="col-span-2">
             <span>Won</span>
           </div>
         </div>
         <div className="mt-2">
           {winners.map(({ address, amount }, i) => (
-            <div key={i} className=" grid grid-cols-4 gap-y-4 px-1 mb-2 items-center">
+            <div key={i} className=" grid grid-cols-5 gap-y-4 px-1 mb-2 items-center">
               <div className="col-span-3">
                 <Address address={address} size="lg" />
               </div>
-              <div className="col-span-1">
+              <div className="col-span-2">
                 <Amount showUsdPrice={true} amount={Number(formatEther(amount))} className="text-lg" />
               </div>
             </div>
