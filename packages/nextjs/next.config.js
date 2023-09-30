cf2b4c0f818ed63f273434c232286d0b438bc609
Rinat <rinat@hey.com>
2023-09-30 20:22:37 +0400

    fix: review

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -4,10 +4,10 @@
 const nextConfig = {
   reactStrictMode: true,
   typescript: {
-    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+    ignoreBuildErrors: true,
   },
   eslint: {
-    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+    ignoreDuringBuilds: true,
   },
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
