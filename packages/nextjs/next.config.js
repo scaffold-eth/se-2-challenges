d2e81a37fe609b38fa64c4c37f99fdfa1fae6c60
Rinat <rinat@hey.com>
2023-09-30 14:54:14 +0400

    feat: pre-app-router se-2 backmerge

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -3,12 +3,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Ignoring typescript/eslint errors during build (deploy won't fail even if there are errors)
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  },
+  webpack: config => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
   },
 };
 
