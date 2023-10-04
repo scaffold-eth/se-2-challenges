b897d58357b53706ae709acbce0e8d0d263bfd61
Rinat <rinat@hey.com>
2023-10-04 22:24:42 +0400

    Challenge 1 backmerge v2 (#99)
    * fix 0 price on sepolia network (#85)
    
    * fix: side quest heading (#87)
    
    * Pre-app-router se-2 backmerge (#96)
    
    * fix: lock nextjs
    
    * fix: header lg -> xl
    
    * fix: remove vercel:yolo
    
    * fix: delete mock test file
    
    * remove use of custom light blue color for copy icon (#104)
    
    * Delete light blue color on txhash copy icons
    
    * Increase placeholder readability in dark mode
    
    ---------
    
    Co-authored-by: Shiv Bhonde | shivbhonde.eth <shivbhonde04@gmail.com>
    Co-authored-by: Pankaj Kumar <pkmahan786@gmail.com>
    Co-authored-by: tokodev <pablo@asdloop.com>

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -1,5 +1,6 @@
 import { hardhat } from "wagmi/chains";
 import { CurrencyDollarIcon } from "@heroicons/react/24/outline";
+import { HeartIcon } from "@heroicons/react/24/outline";
 import { SwitchTheme } from "~~/components/SwitchTheme";
 import { Faucet } from "~~/components/scaffold-eth";
 import { useGlobalState } from "~~/services/store/store";
@@ -12,7 +13,7 @@ export const Footer = () => {
   const nativeCurrencyPrice = useGlobalState(state => state.nativeCurrencyPrice);
 
   return (
-    <div className="min-h-0 p-5 mb-11 lg:mb-0">
+    <div className="min-h-0 py-5 px-1 mb-11 lg:mb-0">
       <div>
         <div className="fixed flex justify-between items-center w-full z-10 p-4 bottom-0 left-0 pointer-events-none">
           <div className="flex space-x-2 pointer-events-auto">
@@ -30,8 +31,7 @@ export const Footer = () => {
       <div className="w-full">
         <ul className="menu menu-horizontal w-full">
           <div className="flex justify-center items-center gap-2 text-sm w-full">
-            <div>
-              Built with 🏗{" "}
+            <div className="text-center">
               <a
                 href="https://github.com/scaffold-eth/scaffold-eth-2/"
                 target="_blank"
@@ -39,19 +39,24 @@ export const Footer = () => {
                 className="underline underline-offset-2"
               >
                 Scaffold-ETH 2
-              </a>{" "}
-              at 🏰{" "}
-              <a
-                href="https://buidlguidl.com/"
-                target="_blank"
-                rel="noreferrer"
-                className="underline underline-offset-2"
-              >
-                BuidlGuidl
               </a>
             </div>
             <span>·</span>
             <div>
+              <p className="m-0 text-center">
+                Built with <HeartIcon className="inline-block h-4 w-4" /> at 🏰{" "}
+                <a
+                  href="https://buidlguidl.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="underline underline-offset-2"
+                >
+                  BuidlGuidl
+                </a>
+              </p>
+            </div>
+            <span>·</span>
+            <div className="text-center">
               <a
                 href="https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA"
                 target="_blank"
--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -73,9 +73,9 @@ export const Header = () => {
   );
 
   return (
-    <div className="sticky lg:static top-0 navbar bg-primary min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2">
-      <div className="navbar-start w-auto lg:w-1/2">
-        <div className="lg:hidden dropdown" ref={burgerMenuRef}>
+    <div className="sticky xl:static top-0 navbar bg-primary min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2">
+      <div className="navbar-start w-auto xl:w-1/2">
+        <div className="xl:hidden dropdown" ref={burgerMenuRef}>
           <label
             tabIndex={0}
             className={`ml-1 btn btn-ghost ${isDrawerOpen ? "hover:bg-secondary" : "hover:bg-transparent"}`}
@@ -97,7 +97,7 @@ export const Header = () => {
             </ul>
           )}
         </div>
-        <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
+        <Link href="/" passHref className="hidden xl:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex relative w-10 h-10">
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
@@ -106,7 +106,7 @@ export const Header = () => {
             <span className="text-xs">#1: Decentralized Staking App</span>
           </div>
         </Link>
-        <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">{navLinks}</ul>
+        <ul className="hidden xl:flex xl:flex-nowrap menu menu-horizontal px-1 gap-2">{navLinks}</ul>
       </div>
       <div className="navbar-end flex-grow mr-4">
         <RainbowKitCustomConnectButton />
