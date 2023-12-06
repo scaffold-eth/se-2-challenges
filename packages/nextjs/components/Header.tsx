4d5b23470edb4af479bd1217b003720c8b5bdfc7
tokodev <pablo@asdloop.com>
2023-12-06 18:20:41 +0100

    Set Challenge number and title at Header

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -114,7 +114,7 @@ export const Header = () => {
           </div>
           <div className="flex flex-col">
             <span className="font-bold leading-tight">SRE Challenges</span>
-            <span className="text-xs">#X: Challenge Title</span>
+            <span className="text-xs">#6: Multisig Wallet</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">{navLinks}</ul>
