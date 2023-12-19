bc510519335c5f4f2d199057d1bdb96af48fc1e9
Jacob Homanics <32080359+Hotmanics@users.noreply.github.com>
2023-12-18 22:59:24 -0600

    Update test (#116)

--- packages/hardhat/test/Challenge3.ts
+++ packages/hardhat/test/Challenge3.ts
@@ -12,6 +12,9 @@ describe("🚩 Challenge 3: 🎲 Dice Game", function () {
   let riggedRoll: Contract;
   let provider: JsonRpcProvider;
 
+  const rollAmountString = ".002";
+  const rollAmount = ethers.utils.parseEther(rollAmountString);
+
   async function deployContracts() {
     const DiceGame = await ethers.getContractFactory("DiceGame");
     diceGame = await DiceGame.deploy();
@@ -33,7 +36,7 @@ describe("🚩 Challenge 3: 🎲 Dice Game", function () {
   function fundRiggedContract() {
     return deployer.sendTransaction({
       to: riggedRoll.address,
-      value: ethers.utils.parseEther(".002"),
+      value: rollAmount,
     });
   }
 
@@ -56,7 +59,7 @@ describe("🚩 Challenge 3: 🎲 Dice Game", function () {
         break;
       }
 
-      const options = { value: ethers.utils.parseEther("0.002") };
+      const options = { value: rollAmount };
       await diceGame.rollTheDice(options);
     }
     return expectedRoll;
@@ -68,7 +71,7 @@ describe("🚩 Challenge 3: 🎲 Dice Game", function () {
       expect(await riggedRoll.diceGame()).to.equal(diceGame.address);
     });
 
-    it("Should revert if balance is less than .002 ethers", async function () {
+    it(`Should revert if balance is less than ${rollAmountString} ethers`, async function () {
       await expect(riggedRoll.riggedRoll()).to.be.reverted;
     });
 
@@ -77,10 +80,7 @@ describe("🚩 Challenge 3: 🎲 Dice Game", function () {
       await fundRiggedContract();
       const balance = await provider.getBalance(riggedRoll.address);
       console.log("\t", "💲 RiggedRoll balance: ", ethers.utils.formatEther(balance));
-      expect(balance).to.gte(
-        ethers.utils.parseEther(".002"),
-        "Error when expecting DiceGame contract to have >= .002 eth",
-      );
+      expect(balance).to.gte(rollAmount, `Error when expecting DiceGame contract to have >= ${rollAmount} eth`);
     });
   });
 
@@ -90,15 +90,9 @@ describe("🚩 Challenge 3: 🎲 Dice Game", function () {
       const expectedRoll = await getRoll(getRollLessThanFive);
       console.log("\t", "🎲 Expect roll to be less than or equal to 5. Dice Game Roll:", expectedRoll.toNumber());
 
-      const tx = riggedRoll.riggedRoll();
-
-      it("Should emit Roll event", async () => {
-        await expect(tx).to.emit(diceGame, "Roll").withArgs(riggedRoll.address, expectedRoll);
-      });
-
-      it("Should emit Winner event", async () => {
-        await expect(tx).to.emit(diceGame, "Winner");
-      });
+      const tx = await riggedRoll.riggedRoll();
+      await expect(tx).to.emit(diceGame, "Roll").withArgs(riggedRoll.address, rollAmount, expectedRoll);
+      await expect(tx).to.emit(diceGame, "Winner");
     });
 
     it("Should not call diceGame.rollTheDice for a roll > 5", async () => {
