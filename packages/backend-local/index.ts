02e582b9a5e188a4d0f4f142bbb09f14c71b9892
Rinat <rinat@hey.com>
2024-01-07 18:32:54 +0300

    feat: challenge 6 ts local backend

---
+++ packages/backend-local/index.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import { AddressInfo } from "net";
+
+dotenv.config();
+
+type Transaction = {
+  // [TransactionData type from next app]. Didn't add it since not in use
+  [key: string]: any;
+};
+
+const app = express();
+
+const transactions: { [key: string]: Transaction } = {};
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/:key", async (req, res) => {
+  const { key } = req.params;
+  console.log("Get /", key);
+  res.status(200).send(transactions[key] || {});
+});
+
+app.post("/", async (req, res) => {
+  console.log("Post /", req.body);
+  res.send(req.body);
+  const key = `${req.body.address}_${req.body.chainId}`;
+  console.log("key:", key);
+  if (!transactions[key]) {
+    transactions[key] = {};
+  }
+  transactions[key][req.body.hash] = req.body;
+  console.log("transactions", transactions);
+});
+
+const PORT = process.env.PORT || 49832;
+const server = app.listen(PORT, () => {
+  console.log(
+    "HTTP Listening on port:",
+    (server.address() as AddressInfo).port
+  );
+});
