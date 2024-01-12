018666c7a58b427bbce30c1710d70ede94d7beab
Carlos Sánchez <oceanrdn@gmail.com>
2024-01-12 12:32:31 +0100

    Copy cert to the right folder

--- le.sh
+++ le.sh
@@ -3,5 +3,5 @@
 DOMAIN="backend.multisig.holdings"
 
 sudo certbot certonly --standalone -d $DOMAIN --config-dir ~/.certbot/config --logs-dir ~/.certbot/logs --work-dir ~/.certbot/work
-sudo cp -f ~/.certbot/config/live/$DOMAIN/privkey.pem server.key;sudo chmod 0777 server.key
-sudo cp -f ~/.certbot/config/live/$DOMAIN/cert.pem server.cert;sudo chmod 0777 server.cert
+sudo cp -f ~/.certbot/config/live/$DOMAIN/privkey.pem server.key;sudo chmod 0777 dist/server.key
+sudo cp -f ~/.certbot/config/live/$DOMAIN/cert.pem server.cert;sudo chmod 0777 dist/server.cert
