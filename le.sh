6b61e221ddc2984ee2ec2c0cbc6519e5db7f4429
Carlos Sánchez <oceanrdn@gmail.com>
2024-01-22 13:46:06 +0100

    Update le.sh for crontab usage

--- le.sh
+++ le.sh
@@ -2,6 +2,6 @@
 
 DOMAIN="backend.multisig.holdings"
 
-sudo certbot certonly --standalone -d $DOMAIN --config-dir ~/.certbot/config --logs-dir ~/.certbot/logs --work-dir ~/.certbot/work
+sudo certbot certonly --standalone -d $DOMAIN --config-dir ~/.certbot/config --logs-dir ~/.certbot/logs --work-dir ~/.certbot/work --keep-until-expiring
 sudo cp -f ~/.certbot/config/live/$DOMAIN/privkey.pem server.key;sudo chmod 0777 dist/server.key
 sudo cp -f ~/.certbot/config/live/$DOMAIN/cert.pem server.cert;sudo chmod 0777 dist/server.cert
