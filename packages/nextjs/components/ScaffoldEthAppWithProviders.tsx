3f379db37eb7ad6f1e41a32d544090e07bb33f5a
Rinat <rinat@hey.com>
2024-02-01 00:36:49 +0300

    fix: return space-grotesk font

--- packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -26,7 +26,7 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <>
-      <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col min-h-screen font-space-grotesk">
         <Header />
         <main className="relative flex flex-col flex-1">{children}</main>
         <Footer />
