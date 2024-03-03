06e59cd342f8cc7055e630625ba7704a4f1e17d7
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-02 17:54:50 -0800

    Added additional comments

--- packages/hardhat/deploy/01_deploy_vendor.ts
+++ packages/hardhat/deploy/01_deploy_vendor.ts
@@ -37,9 +37,9 @@ const deployVendor: DeployFunction = async function (hre: HardhatRuntimeEnvironm
   // const vendor = await hre.ethers.getContract<Contract>("Vendor", deployer);
   // const vendorAddress = await vendor.getAddress();
   // // Transfer tokens to Vendor
-  // await yourToken.transfer(vendorAddress, hre.ethers.parseEther("1000"));
+  // // await yourToken.transfer(vendorAddress, hre.ethers.parseEther("1000"));
   // // Transfer contract ownership to your frontend address
-  // await vendor.transferOwnership("**YOUR FRONTEND ADDRESS**");
+  // // await vendor.transferOwnership("**YOUR FRONTEND ADDRESS**");
 };
 
 export default deployVendor;
