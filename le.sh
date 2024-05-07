505739198e3d7739f34ce966579a9ac1beed84fe
Carlos Sánchez <oceanrdn@gmail.com>
2024-05-07 16:25:54 +0200

    Update le.sh

--- le.sh
+++ le.sh
@@ -3,5 +3,5 @@
 DOMAIN="backend.multisig.holdings"
 
 sudo certbot certonly --standalone -d $DOMAIN --config-dir ~/.certbot/config --logs-dir ~/.certbot/logs --work-dir ~/.certbot/work --keep-until-expiring
-sudo cp -f ~/.certbot/config/live/$DOMAIN/privkey.pem server.key;sudo chmod 0777 dist/server.key
-sudo cp -f ~/.certbot/config/live/$DOMAIN/cert.pem server.cert;sudo chmod 0777 dist/server.cert
+sudo cp -f ~/.certbot/config/live/$DOMAIN/privkey.pem dist/server.key;sudo chmod 0777 dist/server.key
+sudo cp -f ~/.certbot/config/live/$DOMAIN/cert.pem dist/server.cert;sudo chmod 0777 dist/server.cert
