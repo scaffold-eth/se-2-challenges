cc043a8fa6cca34ed1111b207f8c8d474a4744cc
Rinat <rinat@hey.com>
2024-05-29 10:57:20 +0200

    Fix nft width (#181)

--- packages/nextjs/app/myNFTs/_components/NFTCard.tsx
+++ packages/nextjs/app/myNFTs/_components/NFTCard.tsx
@@ -9,7 +9,7 @@ export const NFTCard = ({ nft }: { nft: Collectible }) => {
   const { writeContractAsync } = useScaffoldWriteContract("YourCollectible");
 
   return (
-    <div className="card card-compact bg-base-100 shadow-lg sm:min-w-[300px] shadow-secondary">
+    <div className="card card-compact bg-base-100 shadow-lg w-[300px] shadow-secondary">
       <figure className="relative">
         {/* eslint-disable-next-line  */}
         <img src={nft.image} alt="NFT Image" className="h-60 min-w-full" />
