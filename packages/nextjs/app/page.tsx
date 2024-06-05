c1ee84ab93b7feb87195533879b57e4dda279a8c
Rinat <rinat@hey.com>
2024-06-05 23:23:17 +0200

    feat: main page

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
@@ -6,16 +8,43 @@ const Home: NextPage = () => {
       <div className="px-5">
         <h1 className="text-center mb-8">
           <span className="block text-2xl mb-2">SpeedRunEthereum</span>
-          <span className="block text-4xl font-bold">Challenge #X: Challenge Title </span>
+          <span className="block text-4xl font-bold">Challenge #7: 🎁 SVG NFT</span>
         </h1>
-        <p className="text-center text-lg">
-          Get started by editing{" "}
-          <code className="italic bg-base-300 text-base font-bold">packages/nextjs/page/app.tsx</code>
-        </p>
-        <p className="text-center text-lg">
-          Edit your smart contract <code className="italic bg-base-300 text-base font-bold">YourContract.sol</code> in{" "}
-          <code className="italic bg-base-300 text-base font-bold">packages/hardhat/contracts</code>
-        </p>
+        <div className="flex flex-col items-center justify-center">
+          <Image
+            src="/hero.png"
+            width="727"
+            height="231"
+            alt="challenge banner"
+            className="rounded-xl border-4 border-primary"
+          />
+          <div className="max-w-3xl text-center text-lg">
+            <p className="text-center text-lg mt-8">
+              🏆 Assuming you have made it this far you should be comfortable with the basics of 🏗 Scaffold-Eth 2
+            </p>
+            <p>⚗️ This challenge is _very_ open-ended!</p>
+            <p>
+              🛠 Take{" "}
+              <Link href={"/loogies"} className="underline">
+                Loogies NFT
+              </Link>{" "}
+              as an example and make your own SVG NFT. Post the minting URL in the chat!
+            </p>
+            <p className="text-center text-lg font-bold">
+              💸 MAIN GOAL: Build an SVG NFT on mainnet and let us ape in.
+            </p>
+            <p>
+              💵 A side goal here is to make something cool enough that people might pay a little ETH for your hardwork
+              on the Solidity!
+            </p>
+            <p>
+              💬 Meet other builders working on this challenge and get help in the{" "}
+              <a href="https://t.me/+mUeITJ5u7Ig0ZWJh" target="_blank" rel="noreferrer" className="underline">
+                🎁 SVG NFT 🎫 Building Cohort
+              </a>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
