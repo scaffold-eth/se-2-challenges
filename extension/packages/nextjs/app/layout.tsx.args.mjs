d34b4dd21653615484855ac56f3c504b3f70e0de
Damian Martinelli <damianmarti@gmail.com>
2024-12-16 20:06:14 -0300

    Challenge 4 migration to extension (#245)

---
+++ extension/packages/nextjs/app/layout.tsx.args.mjs
@@ -0,0 +1,4 @@
+export const metadata = {
+    title: "Challenge #4 | SpeedRunEthereum",
+    description: "Built with 🏗 Scaffold-ETH 2",
+};
---
+++ extension/packages/nextjs/components/Header.tsx.args.mjs
@@ -0,0 +1,15 @@
+export const menuIconImports = `import { KeyIcon, BoltIcon } from "@heroicons/react/24/outline";`;
+
+export const menuObjects = `{
+    label: "DEX",
+    href: "/dex",
+    icon: <KeyIcon className="h-4 w-4" />,
+  },
+  {
+    label: "Events",
+    href: "/events",
+    icon: <BoltIcon className="h-4 w-4" />,
+  }`;
+
+export const logoTitle = "SRE Challenges";
+export const logoSubtitle = "#4 Build a DEX";
