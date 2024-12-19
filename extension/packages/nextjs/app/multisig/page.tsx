720799e0ba1422c44dabc3891484688c52a1fbfd
Rinat <rinat@hey.com>
2024-12-19 11:59:56 +0100

    Challenge 6 extension (#249)
    * feat: challenge 6 extension
    
    * fix: review

---
+++ extension/packages/nextjs/app/multisig/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { type FC } from "react";
+import { TransactionEventItem } from "./_components";
+import { QRCodeSVG } from "qrcode.react";
+import { Address, Balance } from "~~/components/scaffold-eth";
+import {
+  useDeployedContractInfo,
+  useScaffoldEventHistory,
+} from "~~/hooks/scaffold-eth";
+
+const Multisig: FC = () => {
+  const { data: contractInfo } = useDeployedContractInfo("MetaMultiSigWallet");
+
+  const contractAddress = contractInfo?.address;
+
+  const { data: executeTransactionEvents } = useScaffoldEventHistory({
+    contractName: "MetaMultiSigWallet",
+    eventName: "ExecuteTransaction",
+    fromBlock: 0n,
+  });
+
+  return (
+    <div className="flex items-center flex-col flex-grow w-full my-20 gap-8">
+      <div className="flex flex-col gap-4 items-center bg-base-100 shadow-lg shadow-secondary border-8 border-secondary rounded-xl p-6 w-full max-w-lg">
+        <Balance address={contractAddress} />
+        <QRCodeSVG value={contractAddress || ""} size={256} />
+        <Address address={contractAddress} />
+      </div>
+
+      <div className="flex flex-col mt-10 items-center bg-base-100 shadow-lg shadow-secondary border-8 border-secondary rounded-xl p-6 w-full max-w-3xl">
+        <div className="text-xl font-bold my-2">Events:</div>
+        {executeTransactionEvents?.map((txEvent) => (
+          <TransactionEventItem
+            key={txEvent.args.hash}
+            {...(txEvent.args as Required<(typeof txEvent)["args"]>)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Multisig;
---
+++ extension/packages/nextjs/app/pool/page.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import { type FC, useMemo, useState } from "react";
+import { TransactionData, getPoolServerUrl } from "../create/page";
+import { TransactionItem } from "./_components";
+import { useInterval } from "usehooks-ts";
+import { useChainId } from "wagmi";
+import {
+  useDeployedContractInfo,
+  useScaffoldContract,
+  useScaffoldEventHistory,
+  useScaffoldReadContract,
+} from "~~/hooks/scaffold-eth";
+import { useTargetNetwork } from "~~/hooks/scaffold-eth/useTargetNetwork";
+import { notification } from "~~/utils/scaffold-eth";
+
+const Pool: FC = () => {
+  const [transactions, setTransactions] = useState<TransactionData[]>();
+  // const [subscriptionEventsHashes, setSubscriptionEventsHashes] = useState<`0x${string}`[]>([]);
+  const { targetNetwork } = useTargetNetwork();
+  const poolServerUrl = getPoolServerUrl(targetNetwork.id);
+  const { data: contractInfo } = useDeployedContractInfo("MetaMultiSigWallet");
+  const chainId = useChainId();
+  const { data: nonce } = useScaffoldReadContract({
+    contractName: "MetaMultiSigWallet",
+    functionName: "nonce",
+  });
+
+  const { data: eventsHistory } = useScaffoldEventHistory({
+    contractName: "MetaMultiSigWallet",
+    eventName: "ExecuteTransaction",
+    fromBlock: 0n,
+    watch: true,
+  });
+
+  const { data: metaMultiSigWallet } = useScaffoldContract({
+    contractName: "MetaMultiSigWallet",
+  });
+
+  const historyHashes = useMemo(
+    () => eventsHistory?.map((ev) => ev.args.hash) || [],
+    [eventsHistory]
+  );
+
+  useInterval(() => {
+    const getTransactions = async () => {
+      try {
+        const res: { [key: string]: TransactionData } = await (
+          await fetch(`${poolServerUrl}${contractInfo?.address}_${chainId}`)
+        ).json();
+
+        const newTransactions: TransactionData[] = [];
+        // eslint-disable-next-line no-restricted-syntax, guard-for-in
+        for (const i in res) {
+          const validSignatures = [];
+          // eslint-disable-next-line guard-for-in, no-restricted-syntax
+          for (const s in res[i].signatures) {
+            const signer = (await metaMultiSigWallet?.read.recover([
+              res[i].hash as `0x${string}`,
+              res[i].signatures[s],
+            ])) as `0x${string}`;
+
+            const isOwner = await metaMultiSigWallet?.read.isOwner([
+              signer as string,
+            ]);
+
+            if (signer && isOwner) {
+              validSignatures.push({ signer, signature: res[i].signatures[s] });
+            }
+          }
+          const update: TransactionData = { ...res[i], validSignatures };
+          newTransactions.push(update);
+        }
+        setTransactions(newTransactions);
+      } catch (e) {
+        notification.error("Error fetching transactions");
+        console.log(e);
+      }
+    };
+
+    getTransactions();
+  }, 3777);
+
+  const lastTx = useMemo(
+    () =>
+      transactions
+        ?.filter((tx) => historyHashes.includes(tx.hash))
+        .sort((a, b) => (BigInt(a.nonce) < BigInt(b.nonce) ? 1 : -1))[0],
+    [historyHashes, transactions]
+  );
+
+  return (
+    <div className="flex flex-col flex-1 items-center my-20 gap-8">
+      <div className="flex items-center flex-col flex-grow w-full max-w-2xl">
+        <div className="flex flex-col items-center bg-base-100 shadow-lg shadow-secondary border-8 border-secondary rounded-xl p-6 w-full">
+          <div className="text-xl font-bold">Pool</div>
+
+          <div>Nonce: {nonce !== undefined ? `#${nonce}` : "Loading..."}</div>
+
+          <div className="flex flex-col mt-8 gap-4">
+            {transactions === undefined
+              ? "Loading..."
+              : transactions.map((tx) => {
+                  return (
+                    <TransactionItem
+                      key={tx.hash}
+                      tx={tx}
+                      completed={historyHashes.includes(
+                        tx.hash as `0x${string}`
+                      )}
+                      outdated={
+                        lastTx?.nonce != undefined &&
+                        BigInt(tx.nonce) <= BigInt(lastTx?.nonce)
+                      }
+                    />
+                  );
+                })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pool;
