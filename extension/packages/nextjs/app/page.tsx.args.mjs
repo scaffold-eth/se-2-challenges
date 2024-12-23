171a0d82824ad4a43956a09d820a780533a8e021
Damian <damianmarti@gmail.com>
2024-12-23 10:35:30 -0300

    Challenge 7 migration to extension

--- extension/packages/nextjs/app/page.tsx.args.mjs
+++ extension/packages/nextjs/app/page.tsx.args.mjs
@@ -1,27 +1,45 @@
-// CHALLENGE-TODO: Update the title, description, and deliverable to reflect your challenge
+export const imports = `import Image from "next/image";`;
+
 export const description = `
-  <div className="flex items-center flex-col flex-grow mt-4">
-    <div className="px-5 w-[90%]">
-      <h1 className="text-center mb-6">
-        <span className="block text-4xl font-bold">CHALLENGE TITLE</span>
-      </h1>
-      <div className="flex flex-col items-center justify-center">
-        <div className="max-w-3xl">
-          <p className="text-center text-lg mt-8">
-            CHALLENGE DESCRIPTION
-          </p>
-          <p className="text-center text-lg">
-            🌟 The final deliverable is an app that CHALLENGE DELIVERABLE. Deploy your contracts to a
-            testnet then build and upload your app to a public web server. Submit the url on{" "}
-            <a href="https://speedrunethereum.com/" target="_blank" rel="noreferrer" className="underline">
-              SpeedRunEthereum.com
-            </a>{" "}
-            !
-          </p>
+  <div className="flex items-center flex-col flex-grow pt-10">
+      <div className="px-5">
+        <h1 className="text-center mb-6">
+          <span className="block text-2xl mb-2">SpeedRunEthereum</span>
+          <span className="block text-4xl font-bold">Challenge #7: ⚖️ 🎁 SVG NFT</span>
+        </h1>
+        <div className="flex flex-col items-center justify-center">
+          <Image
+            src="/hero.png"
+            width="727"
+            height="231"
+            alt="challenge banner"
+            className="rounded-xl border-4 border-primary"
+          />
+          <div className="max-w-3xl">
+            <p className="mt-8">
+              🎨 Creating on-chain SVG NFTs is an exciting way to leverage the power of smart contracts for generating
+              unique digital art. This challenge will have you build a contract that generates dynamic SVG images
+              directly on the blockchain. Users will be able to mint their own unique NFTs with customizable SVG
+              graphics and metadata.
+            </p>
+            <p>
+              🌟 Use{" "}
+              <Link href="/loogies" className="underline">
+                Loogies
+              </Link>{" "}
+              as an example to guide your project. This will provide a solid foundation and inspiration for creating
+              your own dynamic SVG NFTs.
+            </p>
+            <p className="mt-8">
+              💬 Meet other builders working on this challenge and get help in the{" "}
+              <a href="https://t.me/+mUeITJ5u7Ig0ZWJh" target="_blank" rel="noreferrer" className="underline">
+                🎁 SVG NFT 🎫 Building Cohort
+              </a>
+            </p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
 `;
-// CHALLENGE-TODO: Update the externalExtensionName to reflect your challenge
-export const externalExtensionName = "SpeedRunEthereum CHALLENGE TITLE";
+
+export const externalExtensionName = "SpeedRunEthereum Challenge #7";
--- extension/packages/nextjs/components/Header.tsx.args.mjs
+++ extension/packages/nextjs/components/Header.tsx.args.mjs
@@ -1,3 +1,15 @@
+export const menuIconImports = `import { BriefcaseIcon, FaceSmileIcon } from "@heroicons/react/24/outline";`;
+
+export const menuObjects = `{
+    label: "All Loogies",
+    href: "/loogies",
+    icon: <FaceSmileIcon className="h-4 w-4" />,
+  },
+  {
+    label: "Your Loogies",
+    href: "/your-loogies",
+    icon: <BriefcaseIcon className="h-4 w-4" />,
+  }`;
+
 export const logoTitle = "SRE Challenges";
-// CHALLENGE-TODO: Update the logoSubtitle to reflect your challenge title
-export const logoSubtitle = "YOUR CHALLENGE TITLE";
+export const logoSubtitle = "#7 SVG NFT";
