b0bdb5379e27d114e8eb6ad688e058074f0b2e26
Siddhant Kulkarni <kulkarni.sid0809@gmail.com>
2025-01-14 23:02:05 +0530

    Fixed typos in smart contracts

--- packages/hardhat/contracts/ExampleExternalContract.sol
+++ packages/hardhat/contracts/ExampleExternalContract.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;  //Do not change the solidity version as it negativly impacts submission grading
+pragma solidity 0.8.4;  //Do not change the solidity version as it negatively impacts submission grading
 
 contract ExampleExternalContract {
 
