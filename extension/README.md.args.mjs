74a3e0eefa481978bc1c9ebcb5fd63e62ef5c411
pabl0cks <pablo@asdloop.com>
2025-02-12 14:23:35 +0100

    Update readme url to permanent link

--- extension/README.md.args.mjs
+++ extension/README.md.args.mjs
@@ -2,7 +2,7 @@ export const skipQuickStart = true;
 
 export const extraContents = `# 🚩 Challenge 2: 🏵 Token Vendor 🤖
 
-![readme-2](https://github.com/user-attachments/assets/85525249-15c6-47fc-b0f7-18c42773fe39)
+![readme-2](https://raw.githubusercontent.com/scaffold-eth/se-2-challenges/challenge-2-token-vendor/extension/packages/nextjs/public/hero.png)
 
 🤖 Smart contracts are kind of like "always on" _vending machines_ that **anyone** can access. Let's make a decentralized, digital currency. Then, let's build an unstoppable vending machine that will buy and sell the currency. We'll learn about the "approve" pattern for ERC20s and how contract to contract interactions work.
 
