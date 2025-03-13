376518ba865abb2e02a5591b9d771e67036975a6
escottalexander@gmail.com <escottalexander@gmail.com>
2025-03-12 20:02:46 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Base Challenge Extension Template
+> ⚠️ This is not meant to be used as an extension by itself. Follow instructions [here](https://github.com/scaffold-eth/se-2-challenges/blob/main/README.md#-contributing-guide-and-hints-to-create-new-challenges) to see how to use this set of files.
+
+You can use these files to help set up a new extension that matches the style and feel of [SpeedRunEthereum](https://speedrunethereum.com/).
+
+Simply copy the files on top of an in-development extension directly after running `yarn cli` while following these [instructions](https://docs.scaffoldeth.io/extensions/createExtensions).
---
+++ extension/packages/nextjs/app/page.tsx.args.mjs
@@ -0,0 +1,35 @@
+export const imports = `import Image from "next/image";`;
+// CHALLENGE-TODO: Update the title, description, and challengeDeliverable to reflect your challenge
+export const description = `
+  <div className="flex items-center flex-col flex-grow mt-4">
+    <div className="px-5 w-[90%]">
+      <h1 className="text-center mb-6">
+        <span className="block text-4xl font-bold">{challengeTitle}</span>
+      </h1>
+      <div className="flex flex-col items-center justify-center">
+        <Image
+          src="/hero.png"
+          width="727"
+          height="231"
+          alt="challenge banner"
+          className="rounded-xl border-4 border-primary"
+        />
+        <div className="max-w-3xl">
+          <p className="text-center text-lg mt-8">
+            {challengeDescription}
+          </p>
+          <p className="text-center text-lg">
+            🌟 The final deliverable is an app that {challengeDeliverable}. Deploy your contracts to a
+            testnet then build and upload your app to a public web server. Submit the url on{" "}
+            <a href="https://speedrunethereum.com/" target="_blank" rel="noreferrer" className="underline">
+              SpeedRunEthereum.com
+            </a>{" "}
+            !
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+`;
+// CHALLENGE-TODO: Update the externalExtensionName to reflect your challenge
+export const externalExtensionName = "SpeedRunEthereum YOUR CHALLENGE TITLE";
