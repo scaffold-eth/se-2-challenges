f8c517d1cae4a922282bb532196be56c880bc269
escottalexander@gmail.com <escottalexander@gmail.com>
2025-03-20 14:28:45 -0400

    remove bracket variables

--- extension/packages/nextjs/app/page.tsx.args.mjs
+++ extension/packages/nextjs/app/page.tsx.args.mjs
@@ -1,17 +1,17 @@
-// CHALLENGE-TODO: Update the title, description, and challengeDeliverable to reflect your challenge
+// CHALLENGE-TODO: Update the title, description, and deliverable to reflect your challenge
 export const description = `
   <div className="flex items-center flex-col flex-grow mt-4">
     <div className="px-5 w-[90%]">
       <h1 className="text-center mb-6">
-        <span className="block text-4xl font-bold">{challengeTitle}</span>
+        <span className="block text-4xl font-bold">CHALLENGE TITLE</span>
       </h1>
       <div className="flex flex-col items-center justify-center">
         <div className="max-w-3xl">
           <p className="text-center text-lg mt-8">
-            {challengeDescription}
+            CHALLENGE DESCRIPTION
           </p>
           <p className="text-center text-lg">
-            🌟 The final deliverable is an app that {challengeDeliverable}. Deploy your contracts to a
+            🌟 The final deliverable is an app that CHALLENGE DELIVERABLE. Deploy your contracts to a
             testnet then build and upload your app to a public web server. Submit the url on{" "}
             <a href="https://speedrunethereum.com/" target="_blank" rel="noreferrer" className="underline">
               SpeedRunEthereum.com
@@ -24,4 +24,4 @@ export const description = `
   </div>
 `;
 // CHALLENGE-TODO: Update the externalExtensionName to reflect your challenge
-export const externalExtensionName = "SpeedRunEthereum YOUR CHALLENGE TITLE";
+export const externalExtensionName = "SpeedRunEthereum CHALLENGE TITLE";
