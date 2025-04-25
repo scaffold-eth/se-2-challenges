83f0d48a7c8fec6b8e7caa033872295135c5ba43
phipsae <philip.krause88@yahoo.de>
2025-04-25 13:57:25 -0400

    fill template

--- extension/packages/nextjs/app/layout.tsx.args.mjs
+++ extension/packages/nextjs/app/layout.tsx.args.mjs
@@ -1,5 +1,4 @@
-// CHALLENGE-TODO: Update the metadata to reflect your challenge
 export const metadata = {
-    title: "YOUR CHALLENGE TITLE | SpeedRunEthereum",
+    title: "Challenge #6 | SpeedRunEthereum",
     description: "Built with 🏗 Scaffold-ETH 2",
-  };
+};
--- extension/packages/nextjs/components/Header.tsx.args.mjs
+++ extension/packages/nextjs/components/Header.tsx.args.mjs
@@ -1,3 +1,27 @@
+export const menuIconImports = `import { BanknotesIcon, Bars3Icon, BugAntIcon, EyeIcon, UserIcon } from "@heroicons/react/24/outline"`;
+
+export const menuObjects = `{
+    label: "User",
+    href: "/user",
+    icon: <UserIcon className="h-4 w-4" />,
+  },
+  {
+    label: "Liquidity Provider",
+    href: "/liquidity-provider",
+    icon: <BanknotesIcon className="h-4 w-4" />,
+  },
+  {
+    label: "Oracle",
+    href: "/oracle",
+    icon: <EyeIcon className="h-4 w-4" />,
+  },
+  {
+    label: "Debug Contracts",
+    href: "/debug",
+    icon: <BugAntIcon className="h-4 w-4" />,
+  },`;
+
+
+
 export const logoTitle = "SRE Challenges";
-// CHALLENGE-TODO: Update the logoSubtitle to reflect your challenge title
-export const logoSubtitle = "YOUR CHALLENGE TITLE";
+export const logoSubtitle = "Prediction Markets";
