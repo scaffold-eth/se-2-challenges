281be06f0f71a5c834b41a88382c0955ceb6c540
Rinat <rinatkins@gmail.com>
2025-06-14 21:02:28 +0300

    fix: ocl main page build

--- extension/packages/nextjs/app/page.tsx.args.mjs
+++ extension/packages/nextjs/app/page.tsx.args.mjs
@@ -16,10 +16,10 @@ export const description = `
         />
         <div className="max-w-3xl">
           <p className="text-center text-lg mt-8">
-            💳 Build your own lending and borrowing platform. Let's write a contract that takes collateral and lets you borrow other assets against the value of the collateral. What happens when the collateral changes in value? We will be able to borrow more if it is higher or if it is lower we will also build a system for liquidating the debt position.
+            💳 Build your own lending and borrowing platform. Let&apos;s write a contract that takes collateral and lets you borrow other assets against the value of the collateral. What happens when the collateral changes in value? We will be able to borrow more if it is higher or if it is lower we will also build a system for liquidating the debt position.
           </p>
           <p className="text-center text-lg">
-            🌟 The final deliverable is an app that allows anyone to take out a loan in CORN while making sure it is always backed by it's value in ETH.
+            🌟 The final deliverable is an app that allows anyone to take out a loan in CORN while making sure it is always backed by it&apos;s value in ETH.
             Deploy your contracts to a testnet then build and upload your app to a public web server. Submit the url on{" "}
             <a href="https://speedrunethereum.com/" target="_blank" rel="noreferrer" className="underline">
               SpeedRunEthereum.com
