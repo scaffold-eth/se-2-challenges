0f359ef0cdacf0fbe3fd9d18ba98c4b719f78177
Shiv Bhonde | shivbhonde.eth <shivbhonde04@gmail.com>
2025-06-19 17:30:33 +0530

    fix fonts (#302)

--- extension/packages/nextjs/components/ScaffoldEthAppWithProviders.tsx.args.mjs
+++ extension/packages/nextjs/components/ScaffoldEthAppWithProviders.tsx.args.mjs
@@ -1,1 +1,9 @@
-export const globalClassNames = "font-space-grotesk";
+export const providerImports = `
+import { Space_Grotesk } from "next/font/google";
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  variable: "--font-space-grotesk",
+});
+`;
+
+export const globalClassNames = "${spaceGrotesk.variable} font-space-grotesk";
