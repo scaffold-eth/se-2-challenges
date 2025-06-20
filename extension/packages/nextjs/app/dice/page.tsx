866da9d7a5c311959b21fe861ee1f308a333ed6f
Rinat <rinatkins@gmail.com>
2025-06-20 17:21:54 +0300

    Revert "fix: ch3 automining (#253)"
    This reverts commit c053235cad66eeed0689b7d76077caf86e9e8cc1.

--- extension/packages/nextjs/app/dice/page.tsx
+++ extension/packages/nextjs/app/dice/page.tsx
@@ -1,18 +1,9 @@
 "use client";
 
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import type { NextPage } from "next";
 import { Address as AddressType, formatEther, parseEther } from "viem";
-import { createTestClient, http } from "viem";
-import { hardhat } from "viem/chains";
-import { useAccount } from "wagmi";
-import {
-  Amount,
-  Roll,
-  RollEvents,
-  Winner,
-  WinnerEvents,
-} from "~~/app/dice/_components";
+import { Amount, Roll, RollEvents, Winner, WinnerEvents } from "~~/app/dice/_components";
 import { Address } from "~~/components/scaffold-eth";
 import {
   useScaffoldContract,
@@ -29,57 +20,23 @@ const DiceGame: NextPage = () => {
   const [rolls, setRolls] = useState<Roll[]>([]);
   const [winners, setWinners] = useState<Winner[]>([]);
 
-  const { chain } = useAccount();
-
-  const testClient = useMemo(
-    () =>
-      chain?.id === hardhat.id
-        ? createTestClient({
-            chain: hardhat,
-            mode: "hardhat",
-            transport: http(),
-          })
-        : undefined,
-    [chain]
-  );
-
   const videoRef = useRef<HTMLVideoElement>(null);
 
   const [rolled, setRolled] = useState(false);
   const [isRolling, setIsRolling] = useState(false);
 
-  const { data: riggedRollContract } = useScaffoldContract({
-    contractName: "RiggedRoll",
-  });
+  const { data: riggedRollContract } = useScaffoldContract({ contractName: "RiggedRoll" });
   const { data: riggedRollBalance } = useWatchBalance({
     address: riggedRollContract?.address,
   });
-  const { data: prize } = useScaffoldReadContract({
+  const { data: prize } = useScaffoldReadContract({ contractName: "DiceGame", functionName: "prize" });
+
+  const { data: rollsHistoryData, isLoading: rollsHistoryLoading } = useScaffoldEventHistory({
     contractName: "DiceGame",
-    functionName: "prize",
+    eventName: "Roll",
+    fromBlock: 0n,
+    watch: true,
   });
-
-  const { data: rollsHistoryData, isLoading: rollsHistoryLoading } =
-    useScaffoldEventHistory({
-      contractName: "DiceGame",
-      eventName: "Roll",
-      fromBlock: 0n,
-      watch: true,
-    });
-
-  useEffect(() => {
-    if (!testClient) return;
-
-    const interval = setInterval(async () => {
-      try {
-        await testClient.mine({ blocks: 1 });
-      } catch (error) {
-        console.error("Error mining block:", error);
-      }
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [testClient]);
 
   useEffect(() => {
     if (
@@ -94,18 +51,17 @@ const DiceGame: NextPage = () => {
           address: args.player as AddressType,
           amount: Number(args.amount),
           roll: (args.roll as bigint).toString(16).toUpperCase(),
-        })) || []
+        })) || [],
       );
     }
   }, [rolls, rollsHistoryData, rollsHistoryLoading]);
 
-  const { data: winnerHistoryData, isLoading: winnerHistoryLoading } =
-    useScaffoldEventHistory({
-      contractName: "DiceGame",
-      eventName: "Winner",
-      fromBlock: 0n,
-      watch: true,
-    });
+  const { data: winnerHistoryData, isLoading: winnerHistoryLoading } = useScaffoldEventHistory({
+    contractName: "DiceGame",
+    eventName: "Winner",
+    fromBlock: 0n,
+    watch: true,
+  });
 
   useEffect(() => {
     if (
@@ -119,16 +75,14 @@ const DiceGame: NextPage = () => {
         winnerHistoryData?.map(({ args }) => ({
           address: args.winner as AddressType,
           amount: args.amount as bigint,
-        })) || []
+        })) || [],
       );
     }
   }, [winnerHistoryData, winnerHistoryLoading, winners.length]);
 
-  const { writeContractAsync: writeDiceGameAsync, isError: rollTheDiceError } =
-    useScaffoldWriteContract("DiceGame");
+  const { writeContractAsync: writeDiceGameAsync, isError: rollTheDiceError } = useScaffoldWriteContract("DiceGame");
 
-  const { writeContractAsync: writeRiggedRollAsync, isError: riggedRollError } =
-    useScaffoldWriteContract("RiggedRoll");
+  const { writeContractAsync: writeRiggedRollAsync, isError: riggedRollError } = useScaffoldWriteContract("RiggedRoll");
 
   useEffect(() => {
     if (rollTheDiceError || riggedRollError) {
@@ -153,18 +107,12 @@ const DiceGame: NextPage = () => {
 
         <div className="flex flex-col items-center pt-4 max-lg:row-start-1">
           <div className="flex w-full justify-center">
-            <span className="text-xl">
-              Roll a 0, 1, 2, 3, 4 or 5 to win the prize!
-            </span>
+            <span className="text-xl"> Roll a 0, 1, 2, 3, 4 or 5 to win the prize! </span>
           </div>
 
           <div className="flex items-center mt-1">
             <span className="text-lg mr-2">Prize:</span>
-            <Amount
-              amount={prize ? Number(formatEther(prize)) : 0}
-              showUsdPrice
-              className="text-lg"
-            />
+            <Amount amount={prize ? Number(formatEther(prize)) : 0} showUsdPrice className="text-lg" />
           </div>
 
           <button
@@ -174,10 +122,7 @@ const DiceGame: NextPage = () => {
               }
               setIsRolling(true);
               try {
-                await writeDiceGameAsync({
-                  functionName: "rollTheDice",
-                  value: parseEther(ROLL_ETH_VALUE),
-                });
+                await writeDiceGameAsync({ functionName: "rollTheDice", value: parseEther(ROLL_ETH_VALUE) });
               } catch (err) {
                 console.error("Error calling rollTheDice function", err);
               }
@@ -190,16 +135,11 @@ const DiceGame: NextPage = () => {
           <div className="mt-4 pt-2 flex flex-col items-center w-full justify-center border-t-4 border-primary">
             <span className="text-2xl">Rigged Roll</span>
             <div className="flex mt-2 items-center">
-              <span className="mr-2 text-lg">Address:</span>
-              <Address size="lg" address={riggedRollContract?.address} />
+              <span className="mr-2 text-lg">Address:</span> <Address size="lg" address={riggedRollContract?.address} />{" "}
             </div>
             <div className="flex mt-1 items-center">
               <span className="text-lg mr-2">Balance:</span>
-              <Amount
-                amount={Number(riggedRollBalance?.formatted || 0)}
-                showUsdPrice
-                className="text-lg"
-              />
+              <Amount amount={Number(riggedRollBalance?.formatted || 0)} showUsdPrice className="text-lg" />
             </div>
           </div>
           {/* <button
@@ -219,34 +159,16 @@ const DiceGame: NextPage = () => {
           >
             Rigged Roll!
           </button> */}
+
           <div className="flex mt-8">
             {rolled ? (
               isRolling ? (
-                <video
-                  key="rolling"
-                  width={300}
-                  height={300}
-                  loop
-                  src="/rolls/Spin.webm"
-                  autoPlay
-                />
+                <video key="rolling" width={300} height={300} loop src="/rolls/Spin.webm" autoPlay />
               ) : (
-                <video
-                  key="rolled"
-                  width={300}
-                  height={300}
-                  src={`/rolls/${rolls[0]?.roll || "0"}.webm`}
-                  autoPlay
-                />
+                <video key="rolled" width={300} height={300} src={`/rolls/${rolls[0]?.roll || "0"}.webm`} autoPlay />
               )
             ) : (
-              <video
-                ref={videoRef}
-                key="last"
-                width={300}
-                height={300}
-                src={`/rolls/${rolls[0]?.roll || "0"}.webm`}
-              />
+              <video ref={videoRef} key="last" width={300} height={300} src={`/rolls/${rolls[0]?.roll || "0"}.webm`} />
             )}
           </div>
         </div>
