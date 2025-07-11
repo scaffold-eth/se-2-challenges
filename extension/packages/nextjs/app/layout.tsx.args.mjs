5cdfa7d53e5ca559a9c634142fed4f1d02f52fc8
Rinat <rinatkins@gmail.com>
2025-07-11 16:34:57 +0400

    [Beta migration] base-challenge-template (#304)
    * feat: migration to beta
    
    * fix: requirements and install
    
    * fix: checkpoint 0
    
    * fix: yarn chain text

--- extension/packages/nextjs/app/layout.tsx.args.mjs
+++ extension/packages/nextjs/app/layout.tsx.args.mjs
@@ -1,5 +1,16 @@
-// CHALLENGE-TODO: Update the metadata to reflect your challenge
-export const metadata = {
-    title: "YOUR CHALLENGE TITLE | SpeedRunEthereum",
-    description: "Built with 🏗 Scaffold-ETH 2",
-  };
+export const preContent = `
+import { Space_Grotesk } from "next/font/google";
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  variable: "--font-space-grotesk",
+});
+`;
+
+// CHALLENGE-TODO: Update the metadataOverrides to reflect your challenge
+export const metadataOverrides = {
+  title: "YOUR CHALLENGE TITLE | SpeedRunEthereum",
+  description: "Built with 🏗 Scaffold-ETH 2",
+};
+
+export const htmlClassNames = "${spaceGrotesk.variable} font-space-grotesk";
