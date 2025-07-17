f1ef1ca7b359cee8fd5a80b8d7d42c941e70e6d3
Elliott Alexander <escottalexander@gmail.com>
2025-07-17 10:27:08 -0400

    use nullish coalescence

--- extension/packages/nextjs/app/dashboard/_components/CollateralGraph.tsx
+++ extension/packages/nextjs/app/dashboard/_components/CollateralGraph.tsx
@@ -101,8 +101,7 @@ const CollateralGraph = () => {
 
     const collateralAdded: bigint = event?.eventName === "CollateralAdded" ? event?.args.amount || 0n : 0n;
     const collateralWithdrawn: bigint = event?.eventName === "CollateralWithdrawn" ? event?.args.amount || 0n : 0n;
-    const price: bigint =
-      event?.args && event?.args.price ? event?.args.price : getPriceFromEvent(event?.blockNumber, priceEvents);
+    const price: bigint = event?.args?.price ?? getPriceFromEvent(event?.blockNumber, priceEvents);
     const debtAdded: bigint = event?.eventName === "AssetBorrowed" ? event?.args.amount || 0n : 0n;
     const debtRepaid: bigint = event?.eventName === "AssetRepaid" ? event?.args.amount || 0n : 0n;
     const amountForLiquidator: bigint = event?.eventName === "Liquidation" ? event?.args.amountForLiquidator || 0n : 0n;
