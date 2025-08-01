922e5a862fc7446ac41a84ef638487319a7322e8
Carlos Sánchez <oceanrdn@gmail.com>
2025-08-01 19:08:16 +0200

    Update multisig backend endpoint (#341)

--- extension/packages/nextjs/utils/getPoolServerUrl.ts
+++ extension/packages/nextjs/utils/getPoolServerUrl.ts
@@ -1,4 +1,4 @@
 import * as chains from "wagmi/chains";
 
 export const getPoolServerUrl = (id: number) =>
-  id === chains.hardhat.id ? "http://localhost:49832/" : "https://backend.multisig.holdings:49832/";
+  id === chains.hardhat.id ? "http://localhost:49832/" : "https://multisigs.buidlguidl.com:49832/";
