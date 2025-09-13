e2b1be1c575b64513f17594bbe6d64ed83b2ff68
Sherkhan Azimov <sherkhanazim@gmail.com>
2025-09-13 23:01:12 +0200

    update checkpoint 1

--- extension/packages/hardhat/contracts/00_Whitelist/WhitelistOracle.sol
+++ extension/packages/hardhat/contracts/00_Whitelist/WhitelistOracle.sol
@@ -60,13 +60,7 @@ contract WhitelistOracle {
      * @dev Creates a new SimpleOracle instance and adds it to the oracles array.
      * @param _owner The address that will own the newly created oracle and can update its price
      */
-    function addOracle(address _owner) public onlyOwner {
-        SimpleOracle newOracle = new SimpleOracle(_owner);
-        address oracleAddress = address(newOracle);
-
-        oracles.push(newOracle);
-        emit OracleAdded(oracleAddress, _owner);
-    }
+    function addOracle(address _owner) public onlyOwner {}
 
     /**
      * @notice Removes an oracle from the whitelist by its array index (only contract owner)
@@ -74,19 +68,7 @@ contract WhitelistOracle {
      *      Reverts with IndexOutOfBounds, if the provided index is >= oracles.length.
      * @param index The index of the oracle to remove from the oracles array
      */
-    function removeOracle(uint256 index) public onlyOwner {
-        if (index >= oracles.length) revert IndexOutOfBounds();
-
-        address oracleAddress = address(oracles[index]);
-
-        if (index != oracles.length - 1) {
-            oracles[index] = oracles[oracles.length - 1];
-        }
-
-        oracles.pop();
-
-        emit OracleRemoved(oracleAddress);
-    }
+    function removeOracle(uint256 index) public onlyOwner {}
 
     /**
      * @notice Returns the aggregated price from all active oracles using median calculation
@@ -94,31 +76,7 @@ contract WhitelistOracle {
      *      of remaining valid prices. Uses StatisticsUtils for sorting and median calculation.
      * @return The median price from all active oracles
      */
-    function getPrice() public view returns (uint256) {
-        if (oracles.length == 0) revert NoOraclesAvailable();
-
-        // Collect prices and timestamps from all oracles
-        uint256[] memory prices = new uint256[](oracles.length);
-        uint256 validCount = 0; // Count of valid prices
-        uint256 currentTime = block.timestamp;
-
-        for (uint256 i = 0; i < oracles.length; i++) {
-            (uint256 price, uint256 timestamp) = oracles[i].getPrice();
-            // Check if the timestamp is within the last STALE_DATA_WINDOW
-            if (currentTime - timestamp < STALE_DATA_WINDOW) {
-                prices[validCount] = price;
-                validCount++;
-            }
-        }
-
-        uint256[] memory validPrices = new uint256[](validCount);
-        for (uint256 i = 0; i < validCount; i++) {
-            validPrices[i] = prices[i];
-        }
-
-        validPrices.sort();
-        return validPrices.getMedian();
-    }
+    function getPrice() public view returns (uint256) {}
 
     /**
      * @notice Returns the addresses of all oracles that have updated their price within the last STALE_DATA_WINDOW
@@ -127,23 +85,5 @@ contract WhitelistOracle {
      *      for gas optimization.
      * @return An array of addresses representing the currently active oracle contracts
      */
-    function getActiveOracleNodes() public view returns (address[] memory) {
-        address[] memory tempNodes = new address[](oracles.length);
-        uint256 count = 0;
-
-        for (uint256 i = 0; i < oracles.length; i++) {
-            (, uint256 timestamp) = oracles[i].getPrice();
-            if (timestamp > block.timestamp - STALE_DATA_WINDOW) {
-                tempNodes[count] = address(oracles[i]);
-                count++;
-            }
-        }
-
-        address[] memory activeNodes = new address[](count);
-        for (uint256 j = 0; j < count; j++) {
-            activeNodes[j] = tempNodes[j];
-        }
-
-        return activeNodes;
-    }
+    function getActiveOracleNodes() public view returns (address[] memory) {}
 }
