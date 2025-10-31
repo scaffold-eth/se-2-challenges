0123bc90f3cc3cca75be664474df0976d193c632
Elliott Alexander <escottalexander@gmail.com>
2025-10-31 13:38:38 -0400

    refactor: remove unused args and providers from ScaffoldEthAppWithProviders

--- extension/packages/nextjs/components/ScaffoldEthAppWithProviders.tsx.args.mjs
+++ extension/packages/nextjs/components/ScaffoldEthAppWithProviders.tsx.args.mjs
@@ -1,9 +1,3 @@
 // Reference the example args file: https://github.com/scaffold-eth/create-eth-extensions/blob/example/extension/packages/nextjs/components/ScaffoldEthAppWithProviders.tsx.args.mjs
 
-// Default args:
-export const preContent = `import { MonitorAndTriggerTx } from "./MonitorAndTriggerTx";`;
 export const globalClassNames = "font-space-grotesk";
-export const extraProviders = {
-    "MonitorAndTriggerTx": {},
-};
-export const overrideProviders = {};
