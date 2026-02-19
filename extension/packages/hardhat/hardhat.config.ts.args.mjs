47ed68ac0688268780179ce0a1895e40a2891001
Rinat <rinatkins@gmail.com>
2026-02-19 15:02:47 +0300

    fix: hardhat compiler

---
+++ extension/packages/hardhat/hardhat.config.ts.args.mjs
@@ -0,0 +1,24 @@
+export const configOverrides = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            // https://docs.soliditylang.org/en/latest/using-the-compiler.html#optimizer-options
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      mining: {
+        auto: true,
+        interval: 1000,
+      },
+    },
+  },
+};
