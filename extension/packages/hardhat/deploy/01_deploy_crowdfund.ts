7ff36fa18b7733b7e8ffe55a0325077355bab07b
Rinat <rinatkins@gmail.com>
2026-05-22 19:14:17 +0400

    feat: migration to hh v3

--- extension/packages/hardhat/deploy/01_deploy_crowdfund.ts
+++ extension/packages/hardhat/deploy/01_deploy_crowdfund.ts
@@ -1,25 +1,19 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { DeployFunction } from "hardhat-deploy/types";
+import { artifacts, deployScript } from "../rocketh/deploy.js";
 
 /**
  * Deploys a contract named "CrowdFund" using the deployer account.
- *
- * @param hre HardhatRuntimeEnvironment object.
  */
-const deployCrowdFund: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployer } = await hre.getNamedAccounts();
-  const { deploy, get } = hre.deployments;
+export default deployScript(
+  async ({ deploy, get, namedAccounts }) => {
+    const { deployer } = namedAccounts;
 
-  const fundingRecipient = await get("FundingRecipient");
+    const fundingRecipient = get("FundingRecipient");
 
-  await deploy("CrowdFund", {
-    from: deployer,
-    args: [fundingRecipient.address],
-    log: true,
-    autoMine: true,
-  });
-};
-
-export default deployCrowdFund;
-
-deployCrowdFund.tags = ["CrowdFund"];
+    await deploy("CrowdFund", {
+      account: deployer,
+      artifact: artifacts.CrowdFund,
+      args: [fundingRecipient.address],
+    });
+  },
+  { tags: ["CrowdFund"] },
+);
