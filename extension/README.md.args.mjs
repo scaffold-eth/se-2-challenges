8d1298a2387887d9d7da245a2178b7da5ac8bbca
Rinat <rinatkins@gmail.com>
2026-07-13 13:27:59 +0400

    docs: rephrase Speedrun Ethereum mention to keep general statement intact

--- extension/README.md.args.mjs
+++ extension/README.md.args.mjs
@@ -4,7 +4,7 @@ export const extraContents = ({solidityFramework}) => `# 🚩 Challenge: 🏵 To
 
 ![readme](https://raw.githubusercontent.com/scaffold-eth/se-2-challenges/challenge-token-vendor/extension/packages/nextjs/public/hero.png)
 
-🤖 In this Speedrun Ethereum challenge, smart contracts are kind of like "always on" _vending machines_ that **anyone** can access. Let's make a decentralized, digital currency. Then, let's build an unstoppable vending machine that will buy and sell the currency. We'll learn about the "approve" pattern for ERC20s and how contract to contract interactions work.
+🤖 Smart contracts are kind of like "always on" _vending machines_ that **anyone** can access. In this Speedrun Ethereum challenge, let's make a decentralized, digital currency. Then, let's build an unstoppable vending machine that will buy and sell the currency. We'll learn about the "approve" pattern for ERC20s and how contract to contract interactions work.
 
 🏵 Create \`YourToken.sol\` smart contract that inherits the **ERC20** token standard from OpenZeppelin. Set your token to \`_mint()\` **1000** (* 10 ** 18) tokens to the \`msg.sender\`. Then create a \`Vendor.sol\` contract that sells your token using a payable \`buyTokens()\` function.
 
